12346f5517cd507870a6e00558aa47308e4905ba
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-06-05 22:31:39 +0400

    Update Blox Fruit.lua

--- Build/Blox Fruit.lua
+++ Build/Blox Fruit.lua
@@ -1,5 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Settings.lua"))()
 local Library = loadstring(game:HttpGet("https://speedhubxv3-library.surge.sh"))() -- i dont care, you can use this library free :)
+local Place = game.PlaceId
+local World1, World2, World3 = Place == 2753915549, Place == 4442272183, Place == 7449423635
 local Window = Library:MakeWindow({
     Title = "Speed Hub X | " .. Version,
     SaveFolder = "Speed Hub X"
@@ -55,5 +57,29 @@ Toggle(Main, "Hop if Admin or Staff", "", true)
 Main:AddSection("Farming")
 Toggle(Main, "Auto Farm Level", "This Can Farm Level!", false)
 Toggle(Main, "Auto Farm Nearest", "This Can Attack Nearest Mob!", false)
-
+if World3 then
+    Toggle(Main, "Auto Pirates Sea", "This Can Attack Rip Indra Or Something!", false)
+elseif World2 then
+    Toggle(Main, "Auto Factory", "This Can Attack Core!", false)
+end
+Main:AddSection("Farming Mastery")
+Toggle(Main, "Farm Mastery", "Only Farm Level, Not All", false)
+Silder(Main, "Matery Health", 0, 100, 25)
+Main:AddSection("Skill Mastery")
+Toggle(Main, "Skill Z", "", true)Toggle(Main, "Skill X", "", true)Toggle(Main, "Skill C", "", true)Toggle(Main, "Skill V", "", true)Toggle(Main, "Skill F", "", true)
+Main:AddSection("Farming Chest")
+Dropdown(Main, "Choose Chest Area", "", {"Mirage Island", "Island Other"}, "Island Other")
+Toggle(Main, "Auto Collect Chest", "Stop Collect Chest if Get God's Chalice or Fist of Darkness", false)
+Toggle(Main, "Auto Hop", "Hop if No Found Chest", false)
+Main:AddSection("Bosses")
+local BossList = Dropdown(Main, "Choose Boss", "", getgenv().TableBoss, "")
+Main:AddButton({Name = "Refersh Boss", Description = "", Callback = function()
+    BossList:Set(getgenv().TableBoss)
+end})
+Toggle(Main, "Auto Attack Boss", "This Can Attack a Mob Bosses!", false)
+Toggle(Main, "Auto Attack Boss All", "This Can Attack a Mob Bosses All!", false)
+Main:AddButton({Name = "Hop Server", Description = "", Callback = function()Server("Hop1")end})
+Main:AddSection("Material")
+Dropdown(Main, "Choose Material", "", getgenv().MaterialList, "")
+Toggle(Main, "Auto Farm Material", "This Can Farm Material Item!", false)
 return SpeedHubX
