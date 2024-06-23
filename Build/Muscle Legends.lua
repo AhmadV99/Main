d06ca7d9383fce365efaf73082e48b89a9299c42
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-06-23 20:45:45 +0400

    Update Muscle Legends.lua

--- Build/Muscle Legends.lua
+++ Build/Muscle Legends.lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Settings.lua"))()
-local Library = loadstring(game:HttpGet("https://speedhubxv3-library.surge.sh"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Main/main/Library/V3.lua"))()
 local Window = Library:MakeWindow({
     Title = "Speed Hub X | " .. Version,
     SaveFolder = "Speed Hub X"
