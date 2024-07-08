11b3df68bcfcb1781c1850fd7abc44cb1531f680
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-07-09 02:26:08 +0400

    Update The Strongest Battleground.lua

--- Build/The Strongest Battleground.lua
+++ Build/The Strongest Battleground.lua
@@ -1,1 +1,56 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Settings.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Main/main/Library/V3.lua"))()
 
+local Window = Library:MakeWindow({
+    Title = "Speed Hub X | " .. Version,
+    SaveFolder = "Speed Hub X"
+});Window:AddMinimizeButton({
+    Button = {Image = "rbxassetid://16022917595"},
+    Corner = {CornerRadius = UDim.new(0, 5)}
+})
+local Home = Window:MakeTab({"Home", "scan-face"})
+Home:AddDiscordInvite({
+    Name = "Speed Hub X | Official Server",
+    Logo = "rbxassetid://16022917595",
+    Invite = Discord
+})
+local SpeedHubX = {}
+local function Toggle(Tab, Name, Desc, Default)
+  local Ver = Tab:AddToggle({
+    Name = Name,Description = Desc or "",Default = Default,
+    Callback = function(Value)
+      SpeedHubX[Name] = Value
+    end})
+    return Ver
+end
+local function Dropdown(Tab, Name, Desc, Option, Default)
+  local Ver = Tab:AddDropdown({
+    Name = Name,Description = Desc or "",Options = Option,Default = Default,
+    Callback = function(Value)
+      SpeedHubX[Name] = Value
+    end})
+    return Ver
+end
+local function Silder(Tab, Name, Min, Max, Default)
+  local Ver = Tab:AddSlider({
+    Name = Name,Min = Min,Max = Max,Default = Default,
+    Callback = function(Value)
+      SpeedHubX[Name] = Value
+    end})
+    return Ver
+end
+Home:AddSection({"Local Player"})
+Silder(Home, "Set WalkSpeed", 0, 100000, 1000)
+Silder(Home, "Set JumpPower", 0, 100000, 1000)
+Toggle(Home, "Enable WalkSpeed", "This Can Set Walk Speed!", false)
+Toggle(Home, "Enable JumpPower", "This Can Set JumpPower!", false)
+Toggle(Home, "No Clip", "", false)
+Toggle(Home, "Infinits Jump Ping", "", false)
+local Main = Window:MakeTab({"Main", "home"})
+Main:AddSection({"Skill"})
+Toggle(Main, "Auto Ultimate", "This is Meaning Auto Use Ultimate", false)
+Toggle(Main, "Auto Dash", "This is Meaning Auto Use Dash", false)
+Main:AddSection({"Aim"})
+Toggle(Main, "Aimlock", "Aimlock = Lock Camera Player", false)
+Window:SelectTab(2)
+return SpeedHubX
