be3cb8fee9c441410dd43ffbc41f46a9f77410a1
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-07-10 02:24:13 +0400

    Update Muscle Legends.lua

--- Build/Muscle Legends.lua
+++ Build/Muscle Legends.lua
@@ -75,10 +75,12 @@ Toggle(Main, "Auto Punch", "", false)
 Main:AddSection({"Misc"})
 Toggle(Main, "Unlock Fast Punch", "", false)
 Toggle(Main, "Lock Position", "", false)
-if game.Players.LocalPlayer.UserId == 2608992754 then
+if game.Players.LocalPlayer.UserId == 2608992754 or 1724621193 then
   Toggle(Main, "Anti-Knockback", "", false)
 end
 Toggle(Main, "Anti-Rebirths", "", false)
+Toggle(Main, "Anti-Stun", "BETA", false)
+Toggle(Main, "Anti-Busy", "BETA", false)
 Toggle(Main, "Walk On Water", "", false)
 Toggle(Main, "Disable Punch Animation", "", false)
 Main:AddSection({"Brawl"})
