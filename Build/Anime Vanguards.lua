04a72f52f61ed4b53dca7fbecfb2dd0156e9591f
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-09-09 22:24:56 +0400

    Update Anime Vanguards.lua

--- Build/Anime Vanguards.lua
+++ Build/Anime Vanguards.lua
@@ -1,1 +1,151 @@
-.lua
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Main/main/Library/V3.5"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Settings.lua"))()
+
+local Window = Library:Start({
+  ["Name"] = "Speed Hub X | " .. Version,
+  ["SaveFolder"] = "Speed Hub X | Anime Van"
+})
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Lighting = game:GetService("Lighting")
+local TeleportService = game:GetService("TeleportService")
+
+local Player = Players.LocalPlayer
+
+local _isfile = isfile or function(f)return f end
+local _isfolder = isfolder or function(f)return f end
+local _delfolder = delfolder or function(f)return f end
+local _delfile = delfile or function(f)return f end
+
+local SpeedHubX = {}
+
+local Funcs = {} do
+  function Funcs:AddToggle(Section, Name, Content, Default)
+    return Section:Toggle({
+      ["Title"] = Name,
+      ["Content"] = Content,
+      ["Default"] = Default,
+      ["Callback"] = function(Value)
+        SpeedHubX[Name] = Value
+      end,
+      ["Flag"] = "SPD/Toggle/" .. tostring(Name)
+    })
+  end
+
+  function Funcs:AddDropdown(Section, Name, Multi, Options, Default)
+    return Section:Dropdown({
+      ["Title"] = Name,
+      ["Multi"] = Multi,
+      ["Options"] = Options,
+      ["Default"] = Default,
+      ["PlaceHolderText"] = "Select Options",
+      ["Callback"] = function(Value)
+        if type(Value) == "table" then
+          for _, v in pairs(Value) do
+            SpeedHubX[Name] = v
+          end
+        end
+      end
+    })
+  end
+
+  function Funcs:AddSlider(Section, Name, Content, Min, Max, Increment, Default)
+    return Section:Slider({
+      ["Title"] = Name,
+      ["Content"] = Content,
+      ["Min"] = Min,
+      ["Max"] = Max,
+      ["Increment"] = Increment,
+      ["Default"] = Default,
+      ["Callback"] = function(Value)
+        SpeedHubX[Name] = Value
+      end
+    })
+  end
+
+  function Funcs:AddTextbox(Section, Name, Content, Default, ClearText)
+    return Section:TextInput({
+      ["Title"] = Name,
+      ["Content"] = Content,
+      ["PlaceHolderText"] = "Enter your text here...",
+      ["ClearTextOnFocus"] = ClearText,
+      ["Default"] = Default,
+      ["Callback"] = function(Value)
+        SpeedHubX[Name] = Value
+      end
+    })
+  end
+
+  function Funcs:AddButton(Section, Name, Content, Callback)
+    return Section:Button({
+      ["Title"] = Name,
+      ["Content"] = Content,
+      ["Callback"] = Callback
+    })  
+  end
+end
+
+local _home = Window:MakeTab("Home") do
+  local _info = _home:Section({["Title"] = "Information", ["Content"] = ""}) do
+    Funcs:AddButton(_info, "Discord Invite", "Click to copy invite server", function()
+      _setclipboard(Discord)
+    end)
+
+    _info:Seperator("Status")
+
+    local _timeServer = _info:Paragraph({["Title"] = "Time Server", ["Content"] = "" })
+    task.spawn(function()
+      while task.wait(2) do
+        _timeServer:Set({
+          ["Title"] = "Time Server",
+          ["Content"] = tostring(Lighting.TimeOfDay)
+        })
+      end
+    end)
+  end
+
+  local _localplayer = _home:Section({["Title"] = "LocalPlayer", ["Content"] = ""}) do
+    Funcs:AddDropdown(_localplayer, "Set Walk Speed", false, {"100", "200", "300", "400", "500"}, {"300"})
+    Funcs:AddToggle(_localplayer, "Enable Walk Speed", "", false)
+  end
+
+  local _settings = _home:Section({["Title"] = "Settings", ["Content"] = ""}) do
+    Funcs:AddButton(_settings, "Reset Script Saver", "", function()
+      if _isfile("Speed Hub X | Anime Van") then
+        _delfile("Speed Hub X | Anime Van")
+      end
+    end)
+    Funcs:AddButton(_settings, "Rejoin", "", function()
+      TeleportService:Teleport(game.PlaceId, Player)
+    end)
+  end
+end
+
+local _main = Window:MakeTab("Main") do
+  local _Maps = _home:Section({["Title"] = "Maps", ["Content"] = ""}) do
+    _Maps:Seperator("Config")
+    Funcs:AddDropdown(_main, "Choose Map", false, {"Planet Namak", "Send Village", "Double Dungean"}, {"Planet Namak"})
+    Funcs:AddDropdown(_main, "Choose Act Number", false, {"1","2", "3", "4", "5", "6", "Infinite"}, {"1"})
+    Funcs:AddDropdown(_main, "Choose Mode", false, {"Normal","Nightmare"}, {"Normal"})
+    Funcs:AddToggle(_main, "Allow Friends", "", false)
+    _Maps:Seperator("Join")
+    Funcs:AddToggle(_main, "Auto Join Maps", "", false)
+  end
+  local _Game = _home:Section({["Title"] = "Game", ["Content"] = ""}) do
+    Funcs:AddToggle(_Game, "Auto Return To Lobby", "", false)
+    Funcs:AddToggle(_Game, "Auto Click Next", "", false)
+    Funcs:AddToggle(_Game, "Auto Click Retry", "", false)
+  end
+  local _FarmingPlay = _home:Section({["Title"] = "Farming Play", ["Content"] = ""}) do
+    _FarmingPlay:Seperator("Unit")
+    Funcs:AddToggle(_FarmingPlay, "Auto Upgrade Unit", "", false)
+    Funcs:AddToggle(_FarmingPlay, "Auto Sell Unit", "", false)
+    _FarmingPlay:Seperator("Wave")
+    Funcs:AddToggle(_FarmingPlay, "Auto Click Skip Wave", "", false)
+  end
+  local _Summon = _home:Section({["Title"] = "Summon", ["Content"] = ""}) do
+    Funcs:AddToggle(_Summon, "Auto Click Summon X1", "", false)
+    Funcs:AddToggle(_Summon, "Auto Click Summon X10", "", false)
+  end
+end
