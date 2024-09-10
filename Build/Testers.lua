40b6c7fb01154754d069fd27a73a5ac7a62f434c
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-09-10 19:56:44 +0400

    Update Testers.lua

--- Build/Testers.lua
+++ Build/Testers.lua
@@ -149,7 +149,7 @@ local _main = Window:MakeTab("Main") do
     Funcs:AddToggle(_Maps, "Auto Join Maps", "", false)
   end
   local _Game = _main:Section({["Title"] = "Game", ["Content"] = ""}) do
-    Funcs:AddToggle(_Game, "Auto Return To Lobby", "", false)
+    Funcs:AddToggle(_Game, "Auto Click Leave", "", false)
     Funcs:AddToggle(_Game, "Auto Click Next", "", false)
     Funcs:AddToggle(_Game, "Auto Click Retry", "", false)
   end
