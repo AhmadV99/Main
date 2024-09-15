0f44a1e762c894555de1f05626c08806cb1e2927
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-09-15 23:11:18 +0400

    Update Anime Vanguards.lua

--- Build/Anime Vanguards.lua
+++ Build/Anime Vanguards.lua
@@ -216,7 +216,7 @@ local _main = Window:MakeTab("Main") do
     Funcs:AddToggle(_Game, "Auto Click Leave", "", false)
     Funcs:AddToggle(_Game, "Auto Click Next", "", false)
     Funcs:AddToggle(_Game, "Auto Click Retry", "", false)
-    _Maps:Seperator("Reward")
+    _Game:Seperator("Reward")
     Funcs:AddToggle(_Game, "Auto Click Reward In Stage Finished", "", false)
   end
   local _Misc = _main:Section({["Title"] = "Miscellaneous", ["Content"] = ""}) do
