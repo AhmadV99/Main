61c7e6a93ea45f397604de9608209d8f959a8bc9
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-09-17 19:52:45 +0400

    Update Anime Vanguards.lua

--- Build/Anime Vanguards.lua
+++ Build/Anime Vanguards.lua
@@ -190,7 +190,7 @@ local _home = Window:MakeTab("Home") do
       end
     end)
 
-    Funcs:AddToggle(_MoreFPS, "Remove Map", "", false)
+    --Funcs:AddToggle(_MoreFPS, "Remove Map", "", false)
   end
 
   local _settings = _home:Section({["Title"] = "Settings", ["Content"] = ""}) do
@@ -222,8 +222,8 @@ local _main = Window:MakeTab("Main") do
     Funcs:AddToggle(_Game, "Auto Click Leave", "", false)
     Funcs:AddToggle(_Game, "Auto Click Next", "", false)
     Funcs:AddToggle(_Game, "Auto Click Retry", "", false)
-    _Game:Seperator("Reward")
-    Funcs:AddToggle(_Game, "Auto Click Reward In Stage Finished", "", false)
+   -- _Game:Seperator("Reward")
+   -- Funcs:AddToggle(_Game, "Auto Click Reward In Stage Finished", "", false)
   end
   local _Misc = _main:Section({["Title"] = "Miscellaneous", ["Content"] = ""}) do
     _Misc:Seperator("Speed")
