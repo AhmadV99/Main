fe3cd1a05c52284d68f8112d4d72f51c509597d7
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-09-24 22:58:09 +0400

    Update Anime Vanguards.lua

--- Build/Anime Vanguards.lua
+++ Build/Anime Vanguards.lua
@@ -55,6 +55,34 @@ local function GetCountUnits()
   return ListCount
 end
 
+local function GetNameUnits()
+  if game.PlaceId == 16146832113 then
+    local ListName = {}
+    local UnitsFolder = Player.PlayerGui.Windows.Units.Holder.Main.Units
+  
+    if UnitsFolder and #UnitsFolder:GetChildren() > 0 then
+      for _, Unit in pairs(UnitsFolder:GetChildren()) do
+        if Unit:IsA("Frame") then
+          local Holder = Unit:FindFirstChild("Holder")
+          if Holder then
+            local Main = Holder:FindFirstChild("Main")
+            if Main then
+              local UnitName = Main:FindFirstChild("UnitName")
+              if UnitName then
+                table.insert(ListName, UnitName.Text)
+              end
+            end
+          end
+        end
+      end
+    end
+  
+    return ListName
+  end
+
+  return {""}
+end
+
 local SpeedHubX = {}
 
 local Funcs = {} do
@@ -336,6 +364,17 @@ local _main = Window:MakeTab("Main") do
     Funcs:AddToggle(_Claim, "Auto Click Claim Collection Units", "", false)
     Funcs:AddToggle(_Claim, "Auto Click Claim Enemy Index", "", false)
   end
+  local _Traits = _main:Section({["Title"] = "Traits", ["Content"] = ""}) do
+    _Traits:Seperator("Config")
+    Funcs:AddDropdown(_Traits, "Choose WhiteList Traits", false, {"Range", "Range I", "Range II", "Range III", "Swift", "Swift I", "Swift II", "Swift III", "Vigor", "Vigor I", "Vigor II", "Vigor III", "Scholar", "Marksman", "Fortune", "Blitz", "Solar", "Deadeye", "Ethereal", "Monarch"}, {""})
+    Funcs:AddDropdown(_Traits, "Choose Units", false, GetNameUnits(), {""})
+    local UpdateUnit = Funcs:AddButton(_Unit, "Refersh Choose Units", "", function()
+      UpdateUnit:Clear()UpdateUnit:Refresh(GetNameUnits(), {""})
+    end)
+    _Traits:Seperator("Traits")
+    _env.ChancesTraits = _Traits:Paragraph({["Title"] = "You have Chance : N/A"})
+    Funcs:AddToggle(_Traits, "Auto Reroll Units", "If WhiteListed Traits, Will be Stopping Auto Reroll Units", false)
+  end
   local _Ability = _main:Section({["Title"] = "Ability Macro", ["Content"] = ""}) do
     _Ability:Seperator("Create File Config")
     Funcs:AddTextbox(_Ability, "File Name  ", "", "", true)
@@ -349,44 +388,6 @@ local _main = Window:MakeTab("Main") do
     end)
     Funcs:AddButton(_Ability, "Delete On Select File  ", "", function()
       FileSys:DeleteFile("Speed Hub X - Macros/Anime Vanguards/Ability/" .. tostring(SpeedHubX["Select File  "]) .. ".json")
-    end)
-    _Ability:Seperator("Import")
-    _env.ImportMacroName1 = Funcs:AddTextbox(_Ability, "Import Macro Name  ", "", "", true)
-    _env.ImportMacroURL1 = Funcs:AddTextbox(_Ability, "Import Macro URL  ", "Only Url Raw Or Discord Link", "", true)
-    Funcs:AddButton(_Ability, "Create Import Macro  ", "", function()
-      local ImportUrl = SpeedHubX["Import Macro URL  "]
-      local ImportContent = nil
-
-      if string.find(ImportUrl, "https://raw.githubusercontent.com/") or string.find(ImportUrl, "https://cdn.discordapp.com/attachments/") or string.find(ImportUrl, "https://pastebin.com/raw/") then
-        ImportContent = game:HttpGet(ImportUrl)
-      end
-
-      if ImportContent then
-        FileSys:GetFile("Speed Hub X - Macros/Anime Vanguards/Ability/" .. SpeedHubX["Import Macro Name  "] .. ".json", ImportContent)
-        game.StarterGui:SetCore("SendNotification", {Title = "Speed Hub X", Text = "Successfully Imported Ability Macro : " .. SpeedHubX["Import Macro Name  "] .. ".json",Icon = "rbxassetid://0",Duration = 3})
-        ImportMacroName1:Set("")ImportMacroURL1:Set("")
-      else
-        game.StarterGui:SetCore("SendNotification", {Title = "Speed Hub X",Text = "Failed to import macro content.",Icon = "rbxassetid://0",Duration = 3})
-      end
-    end)
-    _Ability:Seperator("Export Macros")
-    Funcs:AddButton(_Ability, "Export Macro", "", function()
-      local Success, Message = pcall(function()
-        local MacroName = SpeedHubX["Select File  "]
-        local FilePath = "Speed Hub X - Macros/Anime Vanguards/Ability/" .. MacroName .. ".json"
-  
-        if FilePath and _readfile(FilePath) then
-          _setclipboard(_readfile(FilePath))
-        else
-          game.StarterGui:SetCore("SendNotification", {Title = "Speed Hub X",Text = "File not found: " .. MacroName .. ".json",Icon = "rbxassetid://0",Duration = 3})
-        end
-      end)
-  
-      if Success then
-        game.StarterGui:SetCore("SendNotification", {Title = "Speed Hub X",Text = "Successfully exported macro: " .. SpeedHubX["Select File  "] .. ".json. Copied to your clipboard!",Icon = "rbxassetid://0",Duration = 3})
-      else
-        game.StarterGui:SetCore("SendNotification", {Title = "Speed Hub X",Text = Message,Icon = "rbxassetid://0",Duration = 3})
-      end
     end)
     _Ability:Seperator("Ability Macro")
     Funcs:AddToggle(_Ability, "Start Ability Record", "", false)
