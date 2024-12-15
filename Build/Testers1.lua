5624d098748ab17803aa45519c5b2c03f5253de0
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-12-16 03:04:52 +0400

    Update Testers1.lua

--- Build/Testers1.lua
+++ Build/Testers1.lua
@@ -510,10 +510,9 @@ local _main = Window:MakeTab("Main") do
     _FarmingDragon:Seperator("Quests")
     Funcs:AddToggle(_FarmingDragon, "Auto Dojo Quests", "BETA (Not Fully)", false)
     Funcs:AddToggle(_FarmingDragon, "Auto Dragon Hunter Quests", "Require: White And Yellow Belt", false)
-    --_FarmingDragon:Seperator("Volcanic Magnet")
-    --Funcs:AddToggle(_FarmingDragon, "Auto Volcanic Magnet", "", false)
     _FarmingDragon:Seperator("Material")
     Funcs:AddToggle(_FarmingDragon, "Auto Blaze Ember", "", false)
+    Funcs:AddToggle(_FarmingDragon, "Auto Volcanic Magnet", "", false)
   end
 end
 
