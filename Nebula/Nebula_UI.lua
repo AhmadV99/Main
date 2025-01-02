cdb0182e32e4c123be5c9b85ac334a34fce24045
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-01-03 01:38:37 +0400

    Update Nebula_UI.lua

--- Nebula/Nebula_UI.lua
+++ Nebula/Nebula_UI.lua
@@ -1,1 +1,32 @@
+repeat wait() until game:IsLoaded()
 
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memejames/elerium-v2-ui-library//main/Library", true))()
+
+local window = library:AddWindow("Nebula", {
+	main_color = Color3.fromRGB(41, 74, 122),
+	min_size = Vector2.new(270, 270), 
+	can_resize = true,
+})
+
+local Script = window:AddTab("Script Editor")
+
+local Console = Script:AddConsole({
+    ["y"] = 100,
+    ["source"] = "Lua",
+})
+
+Console:Set("print('Hello World')")
+
+Script:AddButton("Execute", function()
+    loadstring(Console:Get())()
+end)
+
+Script:AddButton("Clear", function()
+    Console:Set("")
+end)
+
+Script:AddButton("Execute clipboard", function()
+    loadstring(getclipboard())()
+end)
+
+Script:Show()
