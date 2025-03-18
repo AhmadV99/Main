f41aecf7d868603088320cd7550c6ab78c8e57cc
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-03-19 03:02:47 +0400

    Delete Fisch_Data/Event-Tracker.lua

--- Fisch_Data/Event-Tracker.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Lovestorm Eel",
-  "Lovestorm Eel Supercharged",
-  "Megalodon Ancient",
-  "Great White Shark",
-  "Great Hammerhead Shark",
-  "Whale Shark",
-  "Ancient Orcas Pool",
-}
