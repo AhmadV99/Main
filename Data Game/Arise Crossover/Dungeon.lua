b6d110f3f39061197d632c082e665593aaf4c84c
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-04-08 22:02:04 +0400

    Create Dungeon.lua

---
+++ Data Game/Arise Crossover/Dungeon.lua
@@ -0,0 +1,136 @@
+return {
+    DarkDungeon = {
+        Name = "Dark Dungeon", 
+        Icon = "rbxassetid://109237281088402", 
+        Rooms = {
+            Sala1 = {
+                Weight = 4
+            }, 
+            Sala2 = {
+                Weight = 1
+            }, 
+            Sala3 = {
+                Weight = 3
+            }, 
+            Sala4 = {
+                Weight = 5
+            }, 
+            Sala5 = {
+                Weight = 3
+            }
+        }
+    }, 
+    TrainStation = {
+        Name = "Train Station", 
+        Icon = "rbxassetid://107345015777871", 
+        Rooms = {
+            LeftSnake = {
+                Weight = 4
+            }, 
+            RightSnake = {
+                Weight = 3
+            }, 
+            ShortCorridor = {
+                Weight = 6
+            }, 
+            LongCorridor = {
+                Weight = 2
+            }
+        }
+    }, 
+    ImpelDown = {
+        Name = "World Prison", 
+        Icon = "rbxassetid://83730371832524", 
+        Rooms = {
+            Sala1 = {
+                Weight = 9
+            }, 
+            Sala2 = {
+                Weight = 5
+            }, 
+            Sala3 = {
+                Weight = 7
+            }
+        }
+    }, 
+    SnakeLab = {
+        Name = "Snake Lab", 
+        Icon = "rbxassetid://123030113777063", 
+        Rooms = {
+            Sala1 = {
+                Weight = 12, 
+                CantRepeat = true
+            }, 
+            Sala2 = {
+                Weight = 10, 
+                CantRepeat = true
+            }, 
+            Sala3 = {
+                Weight = 3
+            }
+        }
+    }, 
+    SoulSociety = {
+        Name = "Soul City", 
+        Icon = "rbxassetid://128524792572379", 
+        Rooms = {
+            Sala1 = {
+                Weight = 12
+            }, 
+            Sala2 = {
+                Weight = 12
+            }, 
+            Sala3 = {
+                Weight = 12
+            }, 
+            Sala4 = {
+                Weight = 12
+            }
+        }
+    }, 
+    Hotel = {
+        Name = "Hotel", 
+        Icon = "rbxassetid://122744924645571", 
+        Rooms = {
+            Sala1 = {
+                Weight = 12
+            }, 
+            Sala2 = {
+                Weight = 12
+            }, 
+            Sala3 = {
+                Weight = 24
+            }
+        }
+    }, 
+    MagicDungeon = {
+        Name = "Magic Dungeon", 
+        Icon = "rbxassetid://103175144882395", 
+        Rooms = {
+            Sala1 = {
+                Weight = 12
+            }, 
+            Sala2 = {
+                Weight = 12
+            }, 
+            Sala3 = {
+                Weight = 12
+            }
+        }
+    }, 
+    Prison = {
+        Name = "Prison", 
+        Icon = "rbxassetid://109369321032287", 
+        Rooms = {
+            Sala1 = {
+                Weight = 12
+            }, 
+            Sala2 = {
+                Weight = 12
+            }, 
+            Sala3 = {
+                Weight = 12
+            }
+        }
+    }
+};
