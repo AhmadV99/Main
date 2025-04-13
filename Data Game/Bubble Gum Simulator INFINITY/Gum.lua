42f81e7d99e9e134955930fbe56fadeb3a7d54c3
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-04-14 03:09:48 +0400

    Create Gum.lua

---
+++ Data Game/Bubble Gum Simulator INFINITY/Gum.lua
@@ -0,0 +1,156 @@
+local v1 = game:GetService("ReplicatedStorage")
+local v4 = {
+	["Storage"] = 10,
+	["Area"] = "The Overworld",
+	["Image"] = "rbxassetid://134179299358863",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 0
+	}
+}
+v3["Basic Gum"] = v4
+local v5 = {
+	["Storage"] = 45,
+	["Area"] = "The Overworld",
+	["Image"] = "rbxassetid://108550198719106",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 25
+	}
+}
+v3["Stretchy Gum"] = v5
+local v6 = {
+	["Storage"] = 125,
+	["Area"] = "The Overworld",
+	["Image"] = "rbxassetid://82809919925394",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 250
+	}
+}
+v3["Chewy Gum"] = v6
+local v7 = {
+	["Storage"] = 550,
+	["Area"] = "The Overworld",
+	["Image"] = "rbxassetid://83808857441064",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 1500
+	}
+}
+v3["Epic Gum"] = v7
+local v8 = {
+	["Storage"] = 950,
+	["Area"] = "The Overworld",
+	["Image"] = "rbxassetid://94321969730184",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 5000
+	}
+}
+v3["Ultra Gum"] = v8
+local v9 = {
+	["Storage"] = 1750,
+	["Area"] = "The Overworld",
+	["Image"] = "rbxassetid://93588349037042",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 12000
+	}
+}
+v3["Omega Gum"] = v9
+local v10 = {
+	["Storage"] = 4500,
+	["Area"] = "Outer Space",
+	["Image"] = "rbxassetid://128147517686828",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 45000
+	}
+}
+v3["Unreal Gum"] = v10
+local v11 = {
+	["Storage"] = 12500,
+	["Area"] = "Outer Space",
+	["Image"] = "rbxassetid://88359775641835",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 125000
+	}
+}
+v3["Cosmic Gum"] = v11
+local v12 = {
+	["Storage"] = 65500,
+	["Area"] = "Outer Space",
+	["Image"] = "rbxassetid://86107881838160",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 650000
+	}
+}
+v3["XL Gum"] = v12
+local v13 = {
+	["Storage"] = 100000,
+	["Area"] = "Twilight",
+	["Image"] = "rbxassetid://118380171553152",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 1500000
+	}
+}
+v3["Mega Gum"] = v13
+local v14 = {
+	["Storage"] = 225000,
+	["Area"] = "Twilight",
+	["Image"] = "rbxassetid://104358442299675",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 5000000
+	}
+}
+v3["Quantum Gum"] = v14
+local v15 = {
+	["Storage"] = 600000,
+	["Area"] = "Twilight",
+	["Image"] = "rbxassetid://136292734796406",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 35000000
+	}
+}
+v3["Alien Gum"] = v15
+local v16 = {
+	["Storage"] = 900000,
+	["Area"] = "Zen",
+	["Image"] = "rbxassetid://95717273086527",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 150000000
+	}
+}
+v3["Radioactive Gum"] = v16
+local v17 = {
+	["Storage"] = 1500000,
+	["Area"] = "Zen",
+	["Image"] = "rbxassetid://130972694579204",
+	["Cost"] = {
+		["Type"] = "Currency",
+		["Currency"] = "Coins",
+		["Amount"] = 1000000000
+	}
+}
+v3["Experiment #52"] = v17
+return v3
