45f35e6ac3f47a7727a2db2d495755c00a51cb50
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-04-26 23:35:42 +0400

    Update V4.lua

--- Library/V4.lua
+++ Library/V4.lua
@@ -1,3 +1,7 @@
+local message = Instance.new("Message", workspace)
+message.Text = "Hello\nPlease Join Our New Server, More Updates / Supports\nDiscord: discord.gg/speedhubx (Copied)"
+setclipboard("discord.gg/speedhubx")
+
 local Lighting = game:GetService("Lighting")
 local RunService = game:GetService("RunService")
 local LocalPlayer = game:GetService("Players").LocalPlayer
