3e96c75ed3b144f3758b8d9cdbb3bbe40484e826
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-05-24 02:42:22 +0400

    Create Connection.lua

---
+++ Connection.lua
@@ -0,0 +1,60 @@
+local Connection = {}
+Connection.__index = Connection
+
+function Connection.new(Val)
+  local New = setmetatable({}, {
+    _Value = Val or false,
+    _Callbacks = {}
+  })
+
+  return New
+end
+
+function Connection:Connect(Callback)
+  if type(Callback) ~= "function" then return end
+
+  table.insert(self._Callbacks, Callback)
+
+  return function()
+    for i, _CallBack in ipairs(self._Callbacks) do
+      if _CallBack == Callback then
+        table.remove(self._Callbacks, i)
+        break
+      end
+    end
+  end
+end
+
+function Connection:Set(NewVal)
+  if self._Value ~= NewVal then
+    self._Value = NewVal
+    
+    for _, Callback in ipairs(self._Callbacks) do
+      Callback(NewVal)
+    end
+  end
+end
+
+function Connection:Get()
+  return self._Value
+end
+
+function Connection:Fire(...)
+  for _, Callback in ipairs(self._Callbacks) do
+    Callback(...)
+  end
+end
+
+function Connection.Create(Names)
+  assert(type(Names) == "table", "Expected table of names")
+
+  local Events = {}
+
+  for _, name in ipairs(Names) do
+    Events[name] = Connection.new()
+  end
+
+  return Events
+end
+
+return Connection
