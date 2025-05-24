650cc1711eb5fdf6c523f690554d8d9b546da9b1
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-05-24 16:36:11 +0400

    Update Mutations.lua

--- Data Game/Grow A Garden/Mutations.lua
+++ Data Game/Grow A Garden/Mutations.lua
@@ -2,32 +2,32 @@ return {
   ["Chilled"] = {
     Name = "Chilled",
     ValueMulti = 2,
-    Color = Color3.fromRGB(173, 216, 230)
+    Color = Color3.fromRGB(135, 206, 250)
   },
   ["Moonlit"] = {
     Name = "Moonlit",
     ValueMulti = 2,
-    Color = Color3.fromRGB(192, 192, 255)
+    Color = Color3.fromRGB(153, 141, 255)
   },
   ["Twisted"] = {
     Name = "Twisted",
     ValueMulti = 5,
-    Color = Color3.fromRGB(138, 43, 226)
+    Color = Color3.fromRGB(191, 191, 191)
   },
   ["Burnt"] = {
     Name = "Burnt",
     ValueMulti = 5,
-    Color = Color3.fromRGB(139, 69, 19)
+    Color = Color3.fromRGB(40, 40, 40)
   },
   ["Choc"] = {
     Name = "Choc",
     ValueMulti = 2,
-    Color = Color3.fromRGB(123, 63, 0)
+    Color = Color3.fromRGB(92, 64, 51)
   },
   ["Frozen"] = {
     Name = "Frozen",
     ValueMulti = 10,
-    Color = Color3.fromRGB(135, 206, 250)
+    Color = Color3.fromRGB(108, 184, 255)
   },
   ["Wet"] = {
     Name = "Wet",
@@ -37,21 +37,31 @@ return {
   ["Shocked"] = {
     Name = "Shocked",
     ValueMulti = 100,
-    Color = Color3.fromRGB(255, 255, 0)
+    Color = Color3.fromRGB(255, 255, 100)
   },
   ["Bloodlit"] = {
     Name = "Bloodlit",
     ValueMulti = 4,
-    Color = Color3.fromRGB(178, 34, 34)
+    Color = Color3.fromRGB(200, 0, 0)
   },
   ["Zombified"] = {
     Name = "Zombified",
     ValueMulti = 25,
-    Color = Color3.fromRGB(85, 107, 47)
+    Color = Color3.fromRGB(128, 199, 127)
   },
   ["Celestial"] = {
     Name = "Celestial",
     ValueMulti = 120,
-    Color = Color3.fromRGB(255, 255, 255)
+    Color = Color3.fromRGB(255, 0, 255)
+  },
+  ["Disco"] = {
+    Name = "Disco",
+    ValueMulti = 125,
+    Color = Color3.fromRGB(255, 105, 180)
+  },
+  ["Plasma"] = {
+    Name = "Plasma",
+    ValueMulti = 5,
+    Color = Color3.fromRGB(208, 43, 137)
   },
 }
