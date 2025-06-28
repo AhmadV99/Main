95837500e62f738e2f31b6eeeb32471c6bce7684
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-06-28 19:12:41 +0400

    Update Crafting Table.lua

--- Data Game/Grow A Garden/Crafting Table.lua
+++ Data Game/Grow A Garden/Crafting Table.lua
@@ -178,6 +178,31 @@ return {
       [1] = "GearEventWorkbench",
     },
   },
+  ["Mutation Spray Chilled"] = {
+    ["Inputs"] = {
+      [1] = {
+        ["ItemType"] = "SprayBottle",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Cleaning Spray",
+        },
+      },
+      [2] = {
+        ["ItemType"] = "Sprinkler",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Godly Sprinkler",
+        },
+      },
+    },
+    ["MachineTypes"] = {
+      [1] = "GearEventWorkbench",
+    },
+    ["Cost"] = {
+      ["CurrencyType"] = "Sheckles",
+      ["Amount"] = 500000,
+    },
+  },
   ["Flower Froster Sprinkler"] = {
     ["Inputs"] = {
       [1] = {
@@ -302,6 +327,45 @@ return {
       ["Amount"] = 500000,
     },
   },
+  ["Suncoil"] = {
+    ["Inputs"] = {
+      [1] = {
+        ["ItemType"] = "Holdable",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Crocus",
+        },
+      },
+      [2] = {
+        ["ItemType"] = "Holdable",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Daffodil",
+        },
+      },
+      [3] = {
+        ["ItemType"] = "Holdable",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Dandelion",
+        },
+      },
+      [4] = {
+        ["ItemType"] = "Holdable",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Pink Lily",
+        },
+      },
+    },
+    ["MachineTypes"] = {
+      [1] = "SeedEventWorkbench",
+    },
+    ["Cost"] = {
+      ["CurrencyType"] = "Honey",
+      ["Amount"] = 40,
+    },
+  },
   ["Spice Spritzer Sprinkler"] = {
     ["Inputs"] = {
       [1] = {
@@ -362,63 +426,6 @@ return {
       ["Amount"] = 80,
     },
   },
-  ["Crafters Seed Pack"] = {
-    ["Inputs"] = {
-      [1] = {
-        ["ItemType"] = "Seed Pack",
-        ["ItemData"] = {
-          ["AcceptAllTypes"] = false,
-          ["ItemName"] = "Flower Seed Pack",
-        },
-      },
-    },
-    ["MachineTypes"] = {
-      [1] = "SeedEventWorkbench",
-    },
-    ["Cost"] = {
-      ["CurrencyType"] = "Honey",
-      ["Amount"] = 10,
-    },
-  },
-  ["Suncoil"] = {
-    ["Inputs"] = {
-      [1] = {
-        ["ItemType"] = "Holdable",
-        ["ItemData"] = {
-          ["AcceptAllTypes"] = false,
-          ["ItemName"] = "Crocus",
-        },
-      },
-      [2] = {
-        ["ItemType"] = "Holdable",
-        ["ItemData"] = {
-          ["AcceptAllTypes"] = false,
-          ["ItemName"] = "Daffodil",
-        },
-      },
-      [3] = {
-        ["ItemType"] = "Holdable",
-        ["ItemData"] = {
-          ["AcceptAllTypes"] = false,
-          ["ItemName"] = "Dandelion",
-        },
-      },
-      [4] = {
-        ["ItemType"] = "Holdable",
-        ["ItemData"] = {
-          ["AcceptAllTypes"] = false,
-          ["ItemName"] = "Pink Lily",
-        },
-      },
-    },
-    ["MachineTypes"] = {
-      [1] = "SeedEventWorkbench",
-    },
-    ["Cost"] = {
-      ["CurrencyType"] = "Honey",
-      ["Amount"] = 40,
-    },
-  },
   ["Nectar Thorn"] = {
     ["Inputs"] = {
       [1] = {
@@ -456,6 +463,24 @@ return {
     ["Cost"] = {
       ["CurrencyType"] = "Honey",
       ["Amount"] = 20,
+    },
+  },
+  ["Crafters Seed Pack"] = {
+    ["Inputs"] = {
+      [1] = {
+        ["ItemType"] = "Seed Pack",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Flower Seed Pack",
+        },
+      },
+    },
+    ["MachineTypes"] = {
+      [1] = "SeedEventWorkbench",
+    },
+    ["Cost"] = {
+      ["CurrencyType"] = "Honey",
+      ["Amount"] = 10,
     },
   },
   ["Bee Balm"] = {
@@ -513,6 +538,88 @@ return {
     ["Cost"] = {
       ["CurrencyType"] = "Honey",
       ["Amount"] = 20,
+    },
+  },
+  ["Mutation Spray Pollinated"] = {
+    ["Inputs"] = {
+      [1] = {
+        ["ItemType"] = "SprayBottle",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Cleaning Spray",
+        },
+      },
+      [2] = {
+        ["ItemType"] = "Holdable",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Bee Balm",
+        },
+      },
+    },
+    ["MachineTypes"] = {
+      [1] = "GearEventWorkbench",
+    },
+    ["Cost"] = {
+      ["CurrencyType"] = "Honey",
+      ["Amount"] = 25,
+    },
+  },
+  ["Guanabana"] = {
+    ["Inputs"] = {
+      [1] = {
+        ["ItemType"] = "Seed",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Aloe Vera",
+        },
+      },
+      [2] = {
+        ["ItemType"] = "Seed",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Prickly Pear",
+        },
+      },
+      [3] = {
+        ["ItemType"] = "Holdable",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Banana",
+        },
+      },
+    },
+    ["MachineTypes"] = {
+      [1] = "SeedEventWorkbench",
+    },
+    ["Cost"] = {
+      ["CurrencyType"] = "SummerCoins",
+      ["Amount"] = 30,
+    },
+  },
+  ["Aloe Vera"] = {
+    ["Inputs"] = {
+      [1] = {
+        ["ItemType"] = "Seed",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Peace Lily",
+        },
+      },
+      [2] = {
+        ["ItemType"] = "Holdable",
+        ["ItemData"] = {
+          ["AcceptAllTypes"] = false,
+          ["ItemName"] = "Prickly Pear",
+        },
+      },
+    },
+    ["MachineTypes"] = {
+      [1] = "SeedEventWorkbench",
+    },
+    ["Cost"] = {
+      ["CurrencyType"] = "SummerCoins",
+      ["Amount"] = 18,
     },
   },
   ["Mutation Spray Choc"] = {
@@ -583,29 +690,29 @@ return {
       ["Amount"] = 12,
     },
   },
-  ["Mutation Spray Pollinated"] = {
+  ["Peace Lily"] = {
     ["Inputs"] = {
       [1] = {
-        ["ItemType"] = "SprayBottle",
+        ["ItemType"] = "Seed",
         ["ItemData"] = {
           ["AcceptAllTypes"] = false,
-          ["ItemName"] = "Cleaning Spray",
+          ["ItemName"] = "Rafflesia",
         },
       },
       [2] = {
-        ["ItemType"] = "Holdable",
+        ["ItemType"] = "Seed",
         ["ItemData"] = {
           ["AcceptAllTypes"] = false,
-          ["ItemName"] = "Bee Balm",
+          ["ItemName"] = "Cauliflower",
         },
       },
     },
     ["MachineTypes"] = {
-      [1] = "GearEventWorkbench",
+      [1] = "SeedEventWorkbench",
     },
     ["Cost"] = {
-      ["CurrencyType"] = "Honey",
-      ["Amount"] = 25,
+      ["CurrencyType"] = "SummerCoins",
+      ["Amount"] = 3,
     },
   },
   ["Anti Bee Egg"] = {
