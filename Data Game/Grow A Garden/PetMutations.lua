3899b34e4651bcb73f754dbb553dea00a05d7411
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-08-05 14:19:40 +0400

    Create PetMutations.lua

---
+++ Data Game/Grow A Garden/PetMutations.lua
@@ -0,0 +1,220 @@
+return {
+  ["Ascended"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(247, 245, 184),
+    ["Chance"] = 1,
+    ["Boosts"] = {
+    },
+    ["EnumId"] = "n",
+    ["Passives"] = {
+      [1] = "Ascended Pet Mutation",
+    },
+  },
+  ["Inverted"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(128, 128, 128),
+    ["Chance"] = 50,
+    ["Boosts"] = {
+      [1] = {
+        ["BoostAmount"] = 0.5,
+        ["BoostType"] = "PET_XP_BOOST",
+      },
+    },
+    ["EnumId"] = "g",
+    ["Passives"] = {
+      [1] = "Inverted Pet Mutation",
+    },
+  },
+  ["Rideable"] = {
+    ["AvaliableFromMutationMachine"] = false,
+    ["Color"] = Color3.fromRGB(255, 255, 255),
+    ["Chance"] = 0,
+    ["Boosts"] = {
+      [1] = {
+      },
+    },
+    ["EnumId"] = "h",
+    ["Passives"] = {
+    },
+  },
+  ["Radiant"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(248, 108, 38),
+    ["Chance"] = 10,
+    ["Boosts"] = {
+      [1] = {
+      },
+    },
+    ["EnumId"] = "l",
+    ["Passives"] = {
+      [1] = "Radiant Pet Mutation",
+    },
+  },
+  ["Fried"] = {
+    ["AvaliableFromMutationMachine"] = false,
+    ["Color"] = Color3.fromRGB(170, 99, 0),
+    ["Chance"] = 0,
+    ["Boosts"] = {
+    },
+    ["EnumId"] = "q",
+    ["Passives"] = {
+      [1] = "Fried Pet Mutation",
+    },
+  },
+  ["Windy"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(162, 185, 209),
+    ["Chance"] = 30,
+    ["Boosts"] = {
+    },
+    ["EnumId"] = "e",
+    ["Passives"] = {
+      [1] = "Windy Pet Mutation",
+    },
+  },
+  ["Mega"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(255, 90, 90),
+    ["Chance"] = 20,
+    ["Boosts"] = {
+      [1] = {
+        ["BoostAmount"] = 2,
+        ["BoostType"] = "SIZE_MODIFICATION",
+      },
+    },
+    ["EnumId"] = "i",
+    ["Passives"] = {
+      [1] = "Mega Pet Mutation",
+      [2] = "Mega Pet Mutation Bonus",
+    },
+  },
+  ["Shocked"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(255, 255, 100),
+    ["Chance"] = 10,
+    ["Boosts"] = {
+    },
+    ["EnumId"] = "a",
+    ["Passives"] = {
+      [1] = "Shocked Pet Mutation",
+    },
+  },
+  ["Normal"] = {
+    ["AvaliableFromMutationMachine"] = false,
+    ["Color"] = Color3.fromRGB(255, 255, 255),
+    ["Boosts"] = {
+    },
+    ["EnumId"] = "m",
+    ["Passives"] = {
+    },
+  },
+  ["Rainbow"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(4, 175, 236),
+    ["Chance"] = 10,
+    ["Boosts"] = {
+      [1] = {
+        ["BoostAmount"] = 0.2,
+        ["BoostType"] = "PASSIVE_BOOST",
+      },
+    },
+    ["EnumId"] = "c",
+    ["Passives"] = {
+      [1] = "Rainbow Pet Mutation",
+    },
+  },
+  ["IronSkin"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(206, 206, 206),
+    ["Chance"] = 10,
+    ["Boosts"] = {
+      [1] = {
+      },
+    },
+    ["EnumId"] = "k",
+    ["Passives"] = {
+      [1] = "IronSkin Pet Mutation",
+    },
+  },
+  ["Golden"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(255, 225, 0),
+    ["Chance"] = 20,
+    ["Boosts"] = {
+      [1] = {
+        ["BoostAmount"] = 0.1,
+        ["BoostType"] = "PASSIVE_BOOST",
+      },
+    },
+    ["EnumId"] = "b",
+    ["Passives"] = {
+      [1] = "Golden Pet Mutation",
+    },
+  },
+  ["Tiny"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(173, 216, 230),
+    ["Chance"] = 20,
+    ["Boosts"] = {
+      [1] = {
+        ["BoostAmount"] = -0.9,
+        ["BoostType"] = "SIZE_MODIFICATION",
+      },
+    },
+    ["EnumId"] = "j",
+    ["Passives"] = {
+      [1] = "Tiny Pet Mutation",
+      [2] = "Tiny Pet Mutation Bonus",
+    },
+  },
+  ["Tranquil"] = {
+    ["AvaliableFromMutationMachine"] = false,
+    ["Color"] = Color3.fromRGB(94, 247, 247),
+    ["Chance"] = 0,
+    ["Boosts"] = {
+    },
+    ["EnumId"] = "o",
+    ["Passives"] = {
+      [1] = "Tranquil Pet Mutation",
+    },
+  },
+  ["Corrupted"] = {
+    ["AvaliableFromMutationMachine"] = false,
+    ["Color"] = Color3.fromRGB(176, 23, 26),
+    ["Chance"] = 0,
+    ["Boosts"] = {
+    },
+    ["EnumId"] = "p",
+    ["Passives"] = {
+      [1] = "Corrupted Pet Mutation",
+    },
+  },
+  ["Shiny"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(145, 207, 221),
+    ["Chance"] = 100,
+    ["Boosts"] = {
+      [1] = {
+        ["BoostAmount"] = 0.3,
+        ["BoostType"] = "PET_XP_BOOST",
+      },
+    },
+    ["EnumId"] = "d",
+    ["Passives"] = {
+      [1] = "Shiny Pet Mutation",
+    },
+  },
+  ["Frozen"] = {
+    ["AvaliableFromMutationMachine"] = true,
+    ["Color"] = Color3.fromRGB(108, 184, 255),
+    ["Chance"] = 30,
+    ["Boosts"] = {
+      [1] = {
+      },
+    },
+    ["EnumId"] = "f",
+    ["Passives"] = {
+      [1] = "Frozen Pet Mutation",
+    },
+  },
+}
