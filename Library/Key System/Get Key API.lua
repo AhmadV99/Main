9e8bd413e878a20a379a9b24d839b111ab9164f6
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-12-04 18:22:53 +0400

    Update Get Key API.lua

--- Library/Key System/Get Key API.lua
+++ Library/Key System/Get Key API.lua
@@ -100,4 +100,21 @@ return {
       Icon = 137084367454843,
     },
   },
+
+  [7671049560] = { -- The Forge
+    ["Linkvertise"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Linkvertise-jOqPFgJvjYNe",
+      Icon = 122637139040192,
+    },
+
+    ["LootLabs"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Lootlabs-cNYdzGiBsPbi",
+      Icon = 81598390322167,
+    },
+
+    ["Work.ink"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Workink-AaIaLIEtfopE",
+      Icon = 137084367454843,
+    },
+  },
 }
