c6c5e96c5cbee5b5fa28706d63e44bcbfb2243ac
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-01-10 19:08:53 +0400

    Update Get Key API.lua

--- Library/Key System/Get Key API.lua
+++ Library/Key System/Get Key API.lua
@@ -117,4 +117,21 @@ return {
       Icon = 137084367454843,
     },
   },
+
+  [9363735110] = { -- Escape Tsunami For Brainrots
+    ["Linkvertise"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Linkvertise-jOqPFgJvjYNe",
+      Icon = 122637139040192,
+    },
+
+    ["LootLabs"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Lootlabs-cNYdzGiBsPbi",
+      Icon = 81598390322167,
+    },
+
+    ["Work.ink"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Workink-AaIaLIEtfopE",
+      Icon = 137084367454843,
+    },
+  }
 }
