dc4e360a586bba728f9b5bd04f4a780723531598
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-01-17 16:36:25 +0400

    Create BrainrotsModule.lua

---
+++ Data Game/Break a Lucky Block/BrainrotsModule.lua
@@ -0,0 +1,456 @@
+return {
+    ["Lirili Larila"] = {
+        ["Rarity"] = 60000000,
+        ["RarityType"] = "Common",
+        ["CashPerSecond"] = 1,
+        ["Type"] = "None"
+    },
+    ["Trippi Troppi"] = {
+        ["Rarity"] = 50000000,
+        ["RarityType"] = "Common",
+        ["CashPerSecond"] = 2,
+        ["Type"] = "Water"
+    },
+    ["Fluri Flura"] = {
+        ["Rarity"] = 40000000,
+        ["RarityType"] = "Common",
+        ["CashPerSecond"] = 3,
+        ["Type"] = "Air"
+    },
+    ["Pipi Kiwi"] = {
+        ["Rarity"] = 30000000,
+        ["RarityType"] = "Common",
+        ["CashPerSecond"] = 4,
+        ["Type"] = "None"
+    },
+    ["Svinina Bombardino"] = {
+        ["Rarity"] = 20000000,
+        ["RarityType"] = "Common",
+        ["CashPerSecond"] = 6,
+        ["Type"] = "None"
+    },
+    ["Brr Brr Patapim"] = {
+        ["Rarity"] = 10000000,
+        ["RarityType"] = "Common",
+        ["CashPerSecond"] = 8,
+        ["Type"] = "None"
+    },
+    ["Boneca Ambalabu"] = {
+        ["Rarity"] = 9000000,
+        ["RarityType"] = "Uncommon",
+        ["CashPerSecond"] = 10,
+        ["Type"] = "None"
+    },
+    ["Cacto Hipopotamo"] = {
+        ["Rarity"] = 8000000,
+        ["RarityType"] = "Uncommon",
+        ["CashPerSecond"] = 12,
+        ["Type"] = "Water"
+    },
+    ["Gangster Footera"] = {
+        ["Rarity"] = 7000000,
+        ["RarityType"] = "Uncommon",
+        ["CashPerSecond"] = 14,
+        ["Type"] = "None"
+    },
+    ["Chimpanzini Bananini"] = {
+        ["Rarity"] = 6000000,
+        ["RarityType"] = "Uncommon",
+        ["CashPerSecond"] = 18,
+        ["Type"] = "None"
+    },
+    ["Frigo Camelo"] = {
+        ["Rarity"] = 5000000,
+        ["RarityType"] = "Uncommon",
+        ["CashPerSecond"] = 20,
+        ["Type"] = "None"
+    },
+    ["Garamararam"] = {
+        ["Rarity"] = 4000000,
+        ["RarityType"] = "Uncommon",
+        ["CashPerSecond"] = 24,
+        ["Type"] = "None"
+    },
+    ["Talpa Di Fero"] = {
+        ["Rarity"] = 3500000,
+        ["RarityType"] = "Uncommon",
+        ["CashPerSecond"] = 26,
+        ["Type"] = "None"
+    },
+    ["Ta Ta Ta Ta Sahur"] = {
+        ["Rarity"] = 3000000,
+        ["RarityType"] = "Rare",
+        ["CashPerSecond"] = 28,
+        ["Type"] = "None"
+    },
+    ["Tung Sahur"] = {
+        ["Rarity"] = 2700000,
+        ["RarityType"] = "Rare",
+        ["CashPerSecond"] = 32,
+        ["Type"] = "None"
+    },
+    ["Trippi Troppi Troppa Trippa"] = {
+        ["Rarity"] = 2400000,
+        ["RarityType"] = "Rare",
+        ["CashPerSecond"] = 44,
+        ["Type"] = "Water"
+    },
+    ["Bobrito Bandito"] = {
+        ["Rarity"] = 2100000,
+        ["RarityType"] = "Rare",
+        ["CashPerSecond"] = 56,
+        ["Type"] = "None"
+    },
+    ["Trulimero Trulicina"] = {
+        ["Rarity"] = 1800000,
+        ["RarityType"] = "Rare",
+        ["CashPerSecond"] = 68,
+        ["Type"] = "Water"
+    },
+    ["Madudung"] = {
+        ["Rarity"] = 1600000,
+        ["RarityType"] = "Rare",
+        ["CashPerSecond"] = 76,
+        ["Type"] = "None"
+    },
+    ["Bananita Dolphinita"] = {
+        ["Rarity"] = 1400000,
+        ["RarityType"] = "Rare",
+        ["CashPerSecond"] = 84,
+        ["Type"] = "Water"
+    },
+    ["Rhino Toasterino"] = {
+        ["Rarity"] = 900000,
+        ["RarityType"] = "Epic",
+        ["CashPerSecond"] = 96,
+        ["Type"] = "None"
+    },
+    ["Orangutini Ananassini"] = {
+        ["Rarity"] = 800000,
+        ["RarityType"] = "Epic",
+        ["CashPerSecond"] = 108,
+        ["Type"] = "None"
+    },
+    ["Odin Din Din Dun"] = {
+        ["Rarity"] = 700000,
+        ["RarityType"] = "Epic",
+        ["CashPerSecond"] = 120,
+        ["Type"] = "None"
+    },
+    ["Cappuccino Assassino"] = {
+        ["Rarity"] = 600000,
+        ["RarityType"] = "Epic",
+        ["CashPerSecond"] = 132,
+        ["Type"] = "None"
+    },
+    ["Brri Brri Bicus Dicus"] = {
+        ["Rarity"] = 500000,
+        ["RarityType"] = "Epic",
+        ["CashPerSecond"] = 144,
+        ["Type"] = "Air"
+    },
+    ["Glorbo Fruttodrillo"] = {
+        ["Rarity"] = 450000,
+        ["RarityType"] = "Epic",
+        ["CashPerSecond"] = 160,
+        ["Type"] = "Water"
+    },
+    ["Bambini Crostini"] = {
+        ["Rarity"] = 400000,
+        ["RarityType"] = "Epic",
+        ["CashPerSecond"] = 180,
+        ["Type"] = "None"
+    },
+    ["Avocadini Guffo"] = {
+        ["Rarity"] = 350000,
+        ["RarityType"] = "Epic",
+        ["CashPerSecond"] = 200,
+        ["Type"] = "Air"
+    },
+    ["Six Seven"] = {
+        ["Rarity"] = 300000,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 250,
+        ["Type"] = "None"
+    },
+    ["Ballerina Cappuccina"] = {
+        ["Rarity"] = 250000,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 320,
+        ["Type"] = "None"
+    },
+    ["Chef Crabracadabra"] = {
+        ["Rarity"] = 210000,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 480,
+        ["Type"] = "Water"
+    },
+    ["Strawberrelli Flamingelli"] = {
+        ["Rarity"] = 180000,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 640,
+        ["Type"] = "Water"
+    },
+    ["Pandaccini Bananini"] = {
+        ["Rarity"] = 150000,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 820,
+        ["Type"] = "None"
+    },
+    ["Matteo"] = {
+        ["Rarity"] = 130000,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 950,
+        ["Type"] = "None"
+    },
+    ["Burbaloni Luliloli"] = {
+        ["Rarity"] = 110000,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 1040,
+        ["Type"] = "None"
+    },
+    ["Pipi Potato"] = {
+        ["Rarity"] = 90000,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 1100,
+        ["Type"] = "None"
+    },
+    ["Blueberrinni Octopusini"] = {
+        ["Rarity"] = 70000,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 1160,
+        ["Type"] = "None"
+    },
+    ["Bombombini Gusini"] = {
+        ["Rarity"] = 50000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 1300,
+        ["Type"] = "Air"
+    },
+    ["Bombardiro Crocodilo"] = {
+        ["Rarity"] = 40000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 1360,
+        ["Type"] = "Air"
+    },
+    ["Cocofanto Elefanto"] = {
+        ["Rarity"] = 32000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 1520,
+        ["Type"] = "None"
+    },
+    ["Girafa Celeste"] = {
+        ["Rarity"] = 25000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 1760,
+        ["Type"] = "None"
+    },
+    ["Tralalero Tralala"] = {
+        ["Rarity"] = 20000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 2000,
+        ["Type"] = "Water"
+    },
+    ["Orcalero Orcala"] = {
+        ["Rarity"] = 15000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 2240,
+        ["Type"] = "Water"
+    },
+    ["Cavallo Virtuoso"] = {
+        ["Rarity"] = 12000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 2480,
+        ["Type"] = "None"
+    },
+    ["Zibra Zubra Zibralini"] = {
+        ["Rarity"] = 10000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 2720,
+        ["Type"] = "None"
+    },
+    ["Ballerino Lololo"] = {
+        ["Rarity"] = 9000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 3040,
+        ["Type"] = "None"
+    },
+    ["Gorillo Watermelondrillo"] = {
+        ["Rarity"] = 8000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 3200,
+        ["Type"] = "None"
+    },
+    ["Lerulerulerule"] = {
+        ["Rarity"] = 7000,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 3340,
+        ["Type"] = "None"
+    },
+    ["La Vacca Saturno Saturnita"] = {
+        ["Rarity"] = 5000,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 3700,
+        ["Type"] = "None"
+    },
+    ["Pot Hotspot"] = {
+        ["Rarity"] = 4000,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 3850,
+        ["Type"] = "None"
+    },
+    ["Los Tralaleritos"] = {
+        ["Rarity"] = 3000,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 4160,
+        ["Type"] = "Water"
+    },
+    ["Dragon Cannelloni"] = {
+        ["Rarity"] = 2000,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 4640,
+        ["Type"] = "Air"
+    },
+    ["Esok Sekolah"] = {
+        ["Rarity"] = 1500,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 5040,
+        ["Type"] = "None"
+    },
+    ["Job Job Job Sahur"] = {
+        ["Rarity"] = 1000,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 5520,
+        ["Type"] = "None"
+    },
+    ["Strawberry Elephant"] = {
+        ["Rarity"] = 800,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 5920,
+        ["Type"] = "None"
+    },
+    ["Agarrini La Pallini"] = {
+        ["Rarity"] = 600,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 6400,
+        ["Type"] = "None"
+    },
+    ["Karkerkar Kurkur"] = {
+        ["Rarity"] = 500,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 6800,
+        ["Type"] = "None"
+    },
+    ["Tralalita Tralala"] = {
+        ["Rarity"] = 400,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 7200,
+        ["Type"] = "Water"
+    },
+    ["Torrtuginni Dragonfrutini"] = {
+        ["Rarity"] = 300,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 8500,
+        ["Type"] = "None"
+    },
+    ["Espresso Signora"] = {
+        ["Rarity"] = 200,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 8880,
+        ["Type"] = "None"
+    },
+    ["Tigroligre Frutonni"] = {
+        ["Rarity"] = 100,
+        ["RarityType"] = "God",
+        ["CashPerSecond"] = 10000,
+        ["Type"] = "None"
+    },
+    ["Pakrahmatmamat"] = {
+        ["Rarity"] = 80,
+        ["RarityType"] = "God",
+        ["CashPerSecond"] = 12000,
+        ["Type"] = "None"
+    },
+    ["Pakrahmatmatina"] = {
+        ["Rarity"] = 60,
+        ["RarityType"] = "God",
+        ["CashPerSecond"] = 15000,
+        ["Type"] = "None"
+    },
+    ["Chicleteira Bicicleteira"] = {
+        ["Rarity"] = 40,
+        ["RarityType"] = "God",
+        ["CashPerSecond"] = 20000,
+        ["Type"] = "None"
+    },
+    ["Tric Trac Barabum"] = {
+        ["Rarity"] = 0,
+        ["RarityType"] = "Epic",
+        ["CashPerSecond"] = 225,
+        ["Type"] = "None",
+        ["Craft"] = true
+    },
+    ["Lionel Cactuseli"] = {
+        ["Rarity"] = 0,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 1200,
+        ["Type"] = "None",
+        ["Craft"] = true
+    },
+    ["Tirilikalika Tirilikalako"] = {
+        ["Rarity"] = 0,
+        ["RarityType"] = "Legendary",
+        ["CashPerSecond"] = 1250,
+        ["Type"] = "None",
+        ["Craft"] = true
+    },
+    ["Ganganzelli Trulala"] = {
+        ["Rarity"] = 0,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 3500,
+        ["Type"] = "None",
+        ["Craft"] = true
+    },
+    ["Tralaledon"] = {
+        ["Rarity"] = 0,
+        ["RarityType"] = "Mythic",
+        ["CashPerSecond"] = 3600,
+        ["Type"] = "Water",
+        ["Craft"] = true
+    },
+    ["La Grande Combinasion"] = {
+        ["Rarity"] = 0,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 9200,
+        ["Type"] = "None",
+        ["Craft"] = true
+    },
+    ["Triplito Tralaleritos"] = {
+        ["Rarity"] = 0,
+        ["RarityType"] = "Secret",
+        ["CashPerSecond"] = 9500,
+        ["Type"] = "Water",
+        ["Craft"] = true
+    },
+    ["Chicleteirina Bicicleteirina"] = {
+        ["Rarity"] = 0,
+        ["RarityType"] = "God",
+        ["CashPerSecond"] = 25000,
+        ["Type"] = "Water",
+        ["Craft"] = true
+    },
+    ["Chillin Chili"] = {
+        ["Rarity"] = 0,
+        ["RarityType"] = "God",
+        ["CashPerSecond"] = 30000,
+        ["Type"] = "None",
+        ["Craft"] = true
+    },
+    ["Swag Soda"] = {
+        ["Rarity"] = 0,
+        ["RarityType"] = "God",
+        ["CashPerSecond"] = 40000,
+        ["Type"] = "None",
+        ["Craft"] = true
+    }
+}
