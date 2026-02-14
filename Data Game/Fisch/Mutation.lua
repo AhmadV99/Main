46ca77d5d52252e993ca82306096ed715e89c554
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-02-14 23:45:54 +0400

    Update Mutation.lua

--- Data Game/Fisch/Mutation.lua
+++ Data Game/Fisch/Mutation.lua
@@ -16,7 +16,7 @@ return {
             ["PriceMultiply"] = 8.65,
             ["Chance"] = 0,
             ["Display"] = "Igneous",
-            ["Color"] = l_new_0(v4)
+            ["Color"] = Color3.fromRGB(255, 85, 0)
         },
         ["Solar"] = {
             ["PriceMultiply"] = 5.3,
