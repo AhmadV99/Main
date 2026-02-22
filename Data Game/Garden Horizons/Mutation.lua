7ee4620b52f90e15ee81333a6f295a9150aca54d
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-02-22 21:56:06 +0400

    Create Mutation.lua

---
+++ Data Game/Garden Horizons/Mutation.lua
@@ -0,0 +1,72 @@
+return {
+    ["Sandy"] = {
+        ["id"] = "Sandy",
+        ["priceMultiplier"] = 2.5,
+        ["color"] = "#F7D341"
+    },
+    ["Soaked"] = {
+        ["id"] = "Soaked",
+        ["priceMultiplier"] = 1.25,
+        ["color"] = "#4A90D9"
+    },
+    ["Shocked"] = {
+        ["id"] = "Shocked",
+        ["priceMultiplier"] = 4.5,
+        ["color"] = "#FFFF00"
+    },
+    ["Nova"] = {
+        ["id"] = "Nova",
+        ["priceMultiplier"] = 6.5,
+        ["color"] = "#FF6B6B"
+    },
+    ["Starstruck"] = {
+        ["id"] = "Starstruck",
+        ["priceMultiplier"] = 6.5,
+        ["color"] = "#9B59B6"
+    },
+    ["Snowy"] = {
+        ["id"] = "Snowy",
+        ["priceMultiplier"] = 2,
+        ["color"] = "#A5F2F3"
+    },
+    ["Foggy"] = {
+        ["id"] = "Foggy",
+        ["priceMultiplier"] = 1.25,
+        ["color"] = "#C0C0C0"
+    },
+    ["Meteoric"] = {
+        ["id"] = "Meteoric",
+        ["priceMultiplier"] = 10,
+        ["color"] = "#8B0000"
+    },
+    ["Tidal"] = {
+        ["id"] = "Tidal",
+        ["priceMultiplier"] = 2,
+        ["color"] = "#D6EAEA"
+    },
+    ["Flooded"] = {
+        ["id"] = "Flooded",
+        ["priceMultiplier"] = 1.75,
+        ["color"] = "#2E5CB8"
+    },
+    ["Chilled"] = {
+        ["id"] = "Chilled",
+        ["priceMultiplier"] = 1.5,
+        ["color"] = "#B0E0E6"
+    },
+    ["Frostbit"] = {
+        ["id"] = "Frostbit",
+        ["priceMultiplier"] = 3.5,
+        ["color"] = "#E0FFFF"
+    },
+    ["Muddy"] = {
+        ["id"] = "Muddy",
+        ["priceMultiplier"] = 5,
+        ["color"] = "#8B6914"
+    },
+    ["Mossy"] = {
+        ["id"] = "Mossy",
+        ["priceMultiplier"] = 3.5,
+        ["color"] = "#4A6741"
+    }
+};
