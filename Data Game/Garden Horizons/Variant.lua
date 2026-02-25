5121957a68d88f9d41c9898803f90828bbcde6b5
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-02-25 17:37:52 +0400

    Create Variant.lua

---
+++ Data Game/Garden Horizons/Variant.lua
@@ -0,0 +1,14 @@
+return {
+    ["Gold"] = {
+        ["id"] = "Gold",
+        ["priceMultiplier"] = 5,
+        ["chance"] = 2,
+        ["color"] = "#FFD700"
+    },
+    ["Silver"] = {
+        ["id"] = "Silver",
+        ["priceMultiplier"] = 2,
+        ["chance"] = 5,
+        ["color"] = "#C0C0C0"
+    }
+};
