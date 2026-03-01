9758a1c626df30702a7d6f166ff2261cd3f72a71
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-03-01 15:31:45 +0400

    Create SeedShopData.lua

---
+++ Data Game/Garden Horizons/SeedShopData.lua
@@ -0,0 +1,144 @@
+local v1 = {
+    ["RestockInterval"] = 300,
+    ["ShopId"] = "SeedShop"
+}
+local v2 = {
+    ["Carrot"] = {
+        ["Name"] = "Carrot Seed",
+        ["Price"] = 20,
+        ["StockAmount"] = { 7, 23 },
+        ["StockChance"] = 1,
+        ["LayoutOrder"] = 10,
+        ["DisplayInShop"] = true
+    },
+    ["Corn"] = {
+        ["Name"] = "Corn Seed",
+        ["Price"] = 100,
+        ["StockAmount"] = { 1, 3 },
+        ["StockChance"] = 1,
+        ["LayoutOrder"] = 20,
+        ["DisplayInShop"] = true
+    },
+    ["Onion"] = {
+        ["Name"] = "Onion Seed",
+        ["Price"] = 200,
+        ["StockAmount"] = { 4, 10 },
+        ["StockChance"] = 3,
+        ["LayoutOrder"] = 30,
+        ["DisplayInShop"] = true
+    },
+    ["Strawberry"] = {
+        ["Name"] = "Strawberry Seed",
+        ["Price"] = 800,
+        ["StockAmount"] = { 2, 3 },
+        ["StockChance"] = 5,
+        ["LayoutOrder"] = 40,
+        ["DisplayInShop"] = true
+    },
+    ["Mushroom"] = {
+        ["Name"] = "Mushroom Seed",
+        ["Price"] = 1500,
+        ["StockAmount"] = { 1, 3 },
+        ["StockChance"] = 7,
+        ["LayoutOrder"] = 50,
+        ["DisplayInShop"] = true
+    },
+    ["Beetroot"] = {
+        ["Name"] = "Beetroot Seed",
+        ["Price"] = 2500,
+        ["StockAmount"] = { 3, 8 },
+        ["StockChance"] = 8,
+        ["LayoutOrder"] = 60,
+        ["DisplayInShop"] = true
+    },
+    ["Tomato"] = {
+        ["Name"] = "Tomato Seed",
+        ["Price"] = 4000,
+        ["StockAmount"] = { 1, 3 },
+        ["StockChance"] = 10,
+        ["LayoutOrder"] = 70,
+        ["DisplayInShop"] = true
+    },
+    ["Apple"] = {
+        ["Name"] = "Apple Seed",
+        ["Price"] = 7000,
+        ["StockAmount"] = { 1, 2 },
+        ["StockChance"] = 12,
+        ["LayoutOrder"] = 80,
+        ["DisplayInShop"] = true
+    },
+    ["Rose"] = {
+        ["Name"] = "Rose Seed",
+        ["Price"] = 10000,
+        ["StockAmount"] = { 1, 2 },
+        ["StockChance"] = 14,
+        ["LayoutOrder"] = 90,
+        ["DisplayInShop"] = true
+    },
+    ["Wheat"] = {
+        ["Name"] = "Wheat Seed",
+        ["Price"] = 12000,
+        ["StockAmount"] = { 3, 8 },
+        ["StockChance"] = 18,
+        ["LayoutOrder"] = 100,
+        ["DisplayInShop"] = true
+    },
+    ["Banana"] = {
+        ["Name"] = "Banana Seed",
+        ["Price"] = 30000,
+        ["StockAmount"] = { 1, 2 },
+        ["StockChance"] = 25,
+        ["LayoutOrder"] = 110,
+        ["DisplayInShop"] = true
+    },
+    ["Plum"] = {
+        ["Name"] = "Plum Seed",
+        ["Price"] = 60000,
+        ["StockAmount"] = { 1, 2 },
+        ["StockChance"] = 45,
+        ["LayoutOrder"] = 120,
+        ["DisplayInShop"] = true
+    },
+    ["Potato"] = {
+        ["Name"] = "Potato Seed",
+        ["Price"] = 100000,
+        ["StockAmount"] = { 1, 2 },
+        ["StockChance"] = 65,
+        ["LayoutOrder"] = 130,
+        ["DisplayInShop"] = true
+    },
+    ["Cabbage"] = {
+        ["Name"] = "Cabbage Seed",
+        ["Price"] = 150000,
+        ["StockAmount"] = { 2, 4 },
+        ["StockChance"] = 80,
+        ["LayoutOrder"] = 140,
+        ["DisplayInShop"] = true
+    },
+    ["Cherry"] = {
+        ["Name"] = "Cherry Seed",
+        ["Price"] = 1000000,
+        ["StockAmount"] = { 1, 1 },
+        ["StockChance"] = 120,
+        ["LayoutOrder"] = 150,
+        ["DisplayInShop"] = true
+    },
+    ["Bamboo"] = {
+        ["Name"] = "Bamboo Seed",
+        ["Price"] = 175000,
+        ["StockAmount"] = { 1, 3 },
+        ["StockChance"] = 90,
+        ["LayoutOrder"] = 160,
+        ["DisplayInShop"] = false
+    },
+    ["Mango"] = {
+        ["Name"] = "Mango Seed",
+        ["Price"] = 10000000,
+        ["StockAmount"] = { 1, 1 },
+        ["StockChance"] = 180,
+        ["LayoutOrder"] = 170,
+        ["DisplayInShop"] = false
+    }
+}
+v1.ShopData = v2
+return v1
