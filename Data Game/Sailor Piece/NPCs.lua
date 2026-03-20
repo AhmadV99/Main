ad3b62a8390a9ddef61cbe6a193fece607f4c7f4
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-03-20 04:02:39 +0400

    Create NPCs.lua

---
+++ Data Game/Sailor Piece/NPCs.lua
@@ -0,0 +1,189 @@
+return {
+  QuestNPC1 = {
+    ["title"] = "Hollow Hunter",
+    ["requirements"] = {
+      {
+        ["npcType"] = "Hollow",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "hollow_hunt_1",
+    ["recommendedLevel"] = 5000,
+  },
+  QuestNPC2 = {
+    ["title"] = "Desert Bandit Hunter",
+    ["requirements"] = {
+      {
+        ["npcType"] = "DesertBandit",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "desert_hunt_1",
+    ["recommendedLevel"] = 750,
+  },
+  QuestNPC3 = {
+    ["title"] = "Monkey Boss",
+    ["requirements"] = {
+      {
+        ["npcType"] = "MonkeyBoss",
+        ["amount"] = 1,
+      },
+    },
+    ["id"] = "monkey_hunt_2",
+    ["recommendedLevel"] = 500,
+  },
+  QuestNPC4 = {
+    ["title"] = "Sorcerer Hunter",
+    ["requirements"] = {
+      {
+        ["npcType"] = "Sorcerer",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "jjk_hunt_1",
+    ["recommendedLevel"] = 3000,
+  },
+  QuestNPC5 = {
+    ["title"] = "Thief Hunter",
+    ["requirements"] = {
+      {
+        ["npcType"] = "Thief",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "thief_hunt_1",
+    ["recommendedLevel"] = 0,
+  },
+  QuestNPC6 = {
+    ["title"] = "Thief Boss",
+    ["requirements"] = {
+      {
+        ["npcType"] = "ThiefBoss",
+        ["amount"] = 1,
+      },
+    },
+    ["id"] = "boss_hunt_1",
+    ["recommendedLevel"] = 100,
+  },
+  QuestNPC7 = {
+    ["title"] = "Frost Rogue Hunter",
+    ["requirements"] = {
+      {
+        ["npcType"] = "FrostRogue",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "snow_hunt_1",
+    ["recommendedLevel"] = 1500,
+  },
+  QuestNPC8 = {
+    ["title"] = "Blade Masters",
+    ["requirements"] = {
+      {
+        ["npcType"] = "Swordsman",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "swordman_hunt_1",
+    ["recommendedLevel"] = 10000,
+  },
+  QuestNPC9 = {
+    ["title"] = "Desert Bandit Boss",
+    ["requirements"] = {
+      {
+        ["npcType"] = "DesertBoss",
+        ["amount"] = 1,
+      },
+    },
+    ["id"] = "desert_hunt_2",
+    ["recommendedLevel"] = 1000,
+  },
+  QuestNPC10 = {
+    ["title"] = "Monkey Hunter",
+    ["requirements"] = {
+      {
+        ["npcType"] = "Monkey",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "monkey_hunt_1",
+    ["recommendedLevel"] = 250,
+  },
+  QuestNPC11 = {
+    ["title"] = "Curse Hunter",
+    ["requirements"] = {
+      {
+        ["npcType"] = "Curse",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "curse_hunt_1",
+    ["recommendedLevel"] = 7000,
+  },
+  QuestNPC12 = {
+    ["title"] = "Slime Warrior Hunter",
+    ["requirements"] = {
+      {
+        ["npcType"] = "SlimeWarrior",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "slime_warrior_hunt_1",
+    ["recommendedLevel"] = 8000,
+  },
+  QuestNPC13 = {
+    ["title"] = "Quincy Purge",
+    ["requirements"] = {
+      {
+        ["npcType"] = "Quincy",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "quincy_hunt_1",
+    ["recommendedLevel"] = 10750,
+  },
+  QuestNPC14 = {
+    ["title"] = "Academy Challenge",
+    ["requirements"] = {
+      {
+        ["npcType"] = "AcademyTeacher",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "academy_teacher_hunt_1",
+    ["recommendedLevel"] = 9000,
+  },
+  QuestNPC15 = {
+    ["title"] = "Strong Sorcerer Hunter",
+    ["requirements"] = {
+      {
+        ["npcType"] = "StrongSorcerer",
+        ["amount"] = 5,
+      },
+    },
+    ["id"] = "strong_sorcerer_hunt_1",
+    ["recommendedLevel"] = 6250,
+  },
+  QuestNPC16 = {
+    ["title"] = "Winter Warden Boss",
+    ["requirements"] = {
+      {
+        ["npcType"] = "SnowBoss",
+        ["amount"] = 1,
+      },
+    },
+    ["id"] = "snow_hunt_2",
+    ["recommendedLevel"] = 2000,
+  },
+  QuestNPC17 = {
+    ["title"] = "Panda Sorcerer Boss",
+    ["requirements"] = {
+      {
+        ["npcType"] = "PandaMiniBoss",
+        ["amount"] = 1,
+      },
+    },
+    ["id"] = "jjk_hunt_2",
+    ["recommendedLevel"] = 4000,
+  },
+}
