29e62abfb5e9c0b7cc286736d619e29c6242da26
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-05-03 16:48:21 +0400

    Update Get Key API.lua

--- Library/Key System/Get Key API.lua
+++ Library/Key System/Get Key API.lua
@@ -219,4 +219,21 @@ return {
       Icon = 137084367454843,
     },
   },
+
+  [9792947201] = { -- Bite By Night
+    ["Linkvertise"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Linkvertise-jOqPFgJvjYNe",
+      Icon = 122637139040192,
+    },
+
+    ["LootLabs"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Lootlabs-cNYdzGiBsPbi",
+      Icon = 81598390322167,
+    },
+
+    ["Work.ink"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Workink-AaIaLIEtfopE",
+      Icon = 137084367454843,
+    },
+  },
 }
