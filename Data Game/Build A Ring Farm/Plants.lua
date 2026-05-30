4d145945b72f12cdf31ac6f3081c909caca41238
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-05-30 19:22:39 +0400

    Create Plants.lua

---
+++ Data Game/Build A Ring Farm/Plants.lua
@@ -0,0 +1,609 @@
+local t1 = {}
+
+local getMaxStages = function() end
+
+t1.Carrot = {
+	StageGrowTime = 3,
+	Price = 3,
+	RollChance = 80,
+	Cost = 100,
+	Rarity = "Common",
+	Icon = "rbxassetid://126354973861436",
+	MaxStages = getMaxStages("Carrot")
+}
+t1.Beetroot = {
+	StageGrowTime = 3.5,
+	Price = 5,
+	RollChance = 75,
+	Cost = 250,
+	Rarity = "Common",
+	Icon = "rbxassetid://75683972262936",
+	MaxStages = getMaxStages("Beetroot")
+}
+t1.Pumpkin = {
+	StageGrowTime = 4,
+	Price = 8,
+	RollChance = 65,
+	Cost = 500,
+	Rarity = "Common",
+	Icon = "rbxassetid://120071384785086",
+	MaxStages = getMaxStages("Pumpkin")
+}
+t1.Wheat = {
+	StageGrowTime = 5,
+	Price = 12,
+	RollChance = 10,
+	Cost = 600,
+	Rarity = "Uncommon",
+	Icon = "rbxassetid://105958411706292",
+	MaxStages = getMaxStages("Wheat")
+}
+t1.Melon = {
+	StageGrowTime = 6,
+	Price = 18,
+	RollChance = 8,
+	Cost = 1200,
+	Rarity = "Uncommon",
+	Icon = "rbxassetid://89823771870478",
+	MaxStages = getMaxStages("Melon")
+}
+t1.Onion = {
+	StageGrowTime = 6,
+	Price = 20,
+	RollChance = 5,
+	Cost = 2500,
+	Rarity = "Uncommon",
+	Icon = "rbxassetid://76027442310193",
+	MaxStages = getMaxStages("Onion")
+}
+t1.Cantaloupe = {
+	StageGrowTime = 7,
+	Price = 25,
+	RollChance = 4,
+	Cost = 3500,
+	Rarity = "Uncommon",
+	Icon = "rbxassetid://76461849031765",
+	MaxStages = getMaxStages("Cantaloupe")
+}
+t1.Watermelon = {
+	StageGrowTime = 10,
+	Price = 30,
+	RollChance = 3,
+	Cost = 5000,
+	Rarity = "Uncommon",
+	Icon = "rbxassetid://82631082958471",
+	MaxStages = getMaxStages("Watermelon")
+}
+t1.Blueberry = {
+	StageGrowTime = 9,
+	Price = 50,
+	RollChance = 2,
+	Cost = 15000,
+	Rarity = "Rare",
+	Icon = "rbxassetid://101258457596899",
+	MaxStages = getMaxStages("Blueberry")
+}
+t1.Cabbage = {
+	StageGrowTime = 11,
+	Price = 85,
+	RollChance = 1,
+	Cost = 40000,
+	Rarity = "Rare",
+	Icon = "rbxassetid://70494334993203",
+	MaxStages = getMaxStages("Cabbage")
+}
+t1.Grape = {
+	StageGrowTime = 13,
+	Price = 120,
+	RollChance = 0.6,
+	Cost = 65000,
+	Rarity = "Rare",
+	Icon = "rbxassetid://107020576069494",
+	MaxStages = getMaxStages("Grape")
+}
+t1.Peach = {
+	StageGrowTime = 12,
+	Price = 180,
+	RollChance = 0.5,
+	Cost = 120000,
+	Rarity = "Rare",
+	Icon = "rbxassetid://78416136335603",
+	MaxStages = getMaxStages("Peach")
+}
+t1.Bamboo = {
+	StageGrowTime = 10,
+	Price = 160,
+	RollChance = 0.4,
+	Cost = 90000,
+	Rarity = "Rare",
+	Icon = "rbxassetid://127970209139418",
+	MaxStages = getMaxStages("Bamboo")
+}
+t1.Corn = {
+	StageGrowTime = 12,
+	Price = 250,
+	RollChance = 0.45,
+	Cost = 200000,
+	Rarity = "Epic",
+	Icon = "rbxassetid://82869356626181",
+	MaxStages = getMaxStages("Corn")
+}
+t1.Plum = {
+	StageGrowTime = 14,
+	Price = 300,
+	RollChance = 0.3,
+	Cost = 300000,
+	Rarity = "Epic",
+	Icon = "rbxassetid://96266560720274",
+	MaxStages = getMaxStages("Plum")
+}
+t1.Cauliflower = {
+	StageGrowTime = 17,
+	Price = 400,
+	RollChance = 0.2,
+	Cost = 500000,
+	Rarity = "Epic",
+	Icon = "rbxassetid://104954005138604",
+	MaxStages = getMaxStages("Cauliflower")
+}
+t1.Nectarine = {
+	StageGrowTime = 16,
+	Price = 480,
+	RollChance = 0.18,
+	Cost = 600000,
+	Rarity = "Epic",
+	Icon = "rbxassetid://78042422558451",
+	MaxStages = getMaxStages("Nectarine")
+}
+t1.Sunflower = {
+	StageGrowTime = 14,
+	Price = 550,
+	RollChance = 0.15,
+	Cost = 650000,
+	Rarity = "Epic",
+	Icon = "rbxassetid://117253232535322",
+	MaxStages = getMaxStages("Sunflower")
+}
+t1.Citrus = {
+	StageGrowTime = 19,
+	Price = 700,
+	RollChance = 0.1,
+	Cost = 850000,
+	Rarity = "Epic",
+	Icon = "rbxassetid://96654353770726",
+	MaxStages = getMaxStages("Citrus")
+}
+t1["Spring Onion"] = {
+	StageGrowTime = 25,
+	Price = 1200,
+	RollChance = 0.008,
+	Cost = 2500000,
+	Rarity = "Legendary",
+	Icon = "rbxassetid://76427912984352",
+	MaxStages = getMaxStages("Spring Onion")
+}
+t1.Mango = {
+	StageGrowTime = 23,
+	Price = 1600,
+	RollChance = 0.006,
+	Cost = 4000000,
+	Rarity = "Legendary",
+	Icon = "rbxassetid://109503576202591",
+	MaxStages = getMaxStages("Mango")
+}
+t1.Mushroom = {
+	StageGrowTime = 30,
+	Price = 2000,
+	RollChance = 0.004,
+	Cost = 7000000,
+	Rarity = "Legendary",
+	Icon = "rbxassetid://130334558322984",
+	MaxStages = getMaxStages("Mushroom")
+}
+t1.Banana = {
+	StageGrowTime = 27,
+	Price = 2500,
+	RollChance = 0.003,
+	Cost = 9000000,
+	Rarity = "Legendary",
+	Icon = "rbxassetid://110806023176063",
+	MaxStages = getMaxStages("Banana")
+}
+t1.Potato = {
+	StageGrowTime = 30,
+	Price = 2700,
+	RollChance = 0.001,
+	Cost = 15000000,
+	Rarity = "Legendary",
+	Icon = "rbxassetid://106274750283922",
+	MaxStages = getMaxStages("Potato")
+}
+t1.Strawberry = {
+	StageGrowTime = 35,
+	Price = 6000,
+	RollChance = 0.0002,
+	Cost = 30000000,
+	Rarity = "Secret",
+	Icon = "rbxassetid://122861611492960",
+	MaxStages = getMaxStages("Strawberry")
+}
+t1.Glowshroom = {
+	StageGrowTime = 38,
+	Price = 8000,
+	RollChance = 0.00015,
+	Cost = 45000000,
+	Rarity = "Secret",
+	Icon = "rbxassetid://131144371916248",
+	MaxStages = getMaxStages("Glowshroom")
+}
+t1.Beanstalk = {
+	StageGrowTime = 40,
+	Price = 9000,
+	RollChance = 0.0001,
+	Cost = 55000000,
+	Rarity = "Secret",
+	Icon = "rbxassetid://85500096008978",
+	MaxStages = getMaxStages("Beanstalk")
+}
+t1.Tomato = {
+	StageGrowTime = 45,
+	Price = 12000,
+	RollChance = 5E-05,
+	Cost = 100000000,
+	Rarity = "Secret",
+	Icon = "rbxassetid://114056065292567",
+	MaxStages = getMaxStages("Tomato")
+}
+t1.Starfruit = {
+	StageGrowTime = 42,
+	Price = 13000,
+	RollChance = 0,
+	Cost = 130000000,
+	Rarity = "Secret",
+	Icon = "rbxassetid://113159680322143",
+	MaxStages = getMaxStages("Starfruit")
+}
+t1.Apple = {
+	StageGrowTime = 50,
+	Price = 20000,
+	RollChance = 2E-05,
+	Cost = 500000000,
+	Rarity = "Prismatic",
+	Icon = "rbxassetid://114648889310706",
+	MaxStages = getMaxStages("Apple")
+}
+t1.Pineapple = {
+	StageGrowTime = 52,
+	Price = 40000,
+	RollChance = 0,
+	Cost = 800000000,
+	Rarity = "Prismatic",
+	Icon = "rbxassetid://96965977404334",
+	MaxStages = getMaxStages("Pineapple")
+}
+t1["Cherry Blossom"] = {
+	StageGrowTime = 55,
+	Price = 30000,
+	RollChance = 1E-05,
+	Cost = 1500000000,
+	Rarity = "Prismatic",
+	Icon = "rbxassetid://113089617020491",
+	MaxStages = getMaxStages("Cherry Blossom")
+}
+t1["Blood Orange"] = {
+	StageGrowTime = 50,
+	Price = 35000,
+	RollChance = 8E-06,
+	Cost = 1200000000,
+	Rarity = "Prismatic",
+	Icon = "rbxassetid://91735058553113",
+	MaxStages = getMaxStages("Blood Orange")
+}
+t1.Cinnamon = {
+	StageGrowTime = 50,
+	Price = 45000,
+	RollChance = 1E-05,
+	Cost = 1500000000,
+	Rarity = "Prismatic",
+	Icon = "rbxassetid://115406750726940",
+	MaxStages = getMaxStages("Cinnamon")
+}
+t1.Garlic = {
+	StageGrowTime = 53,
+	Price = 50000,
+	RollChance = 5E-06,
+	Cost = 5500000000,
+	Rarity = "Prismatic",
+	Icon = "rbxassetid://125906680429478",
+	MaxStages = getMaxStages("Garlic")
+}
+t1["Diamond Blossom"] = {
+	StageGrowTime = 58,
+	Price = 55000,
+	RollChance = 0,
+	Cost = 2500000000,
+	Rarity = "Divine",
+	Icon = "rbxassetid://122417569106706",
+	MaxStages = getMaxStages("Diamond Blossom")
+}
+t1.Pomegranate = {
+	StageGrowTime = 55,
+	Price = 75000,
+	RollChance = 0,
+	Cost = 4000000000,
+	Rarity = "Divine",
+	Icon = "rbxassetid://103967157117109",
+	MaxStages = getMaxStages("Pomegranate")
+}
+t1["Golden Apple"] = {
+	StageGrowTime = 56,
+	Price = 65000,
+	RollChance = 6E-06,
+	Cost = 5000000000,
+	Rarity = "Divine",
+	Icon = "rbxassetid://139460315400136",
+	MaxStages = getMaxStages("Golden Apple")
+}
+t1["Horned Melon"] = {
+	StageGrowTime = 57,
+	Price = 80000,
+	RollChance = 0,
+	Cost = 3500000000,
+	Rarity = "Divine",
+	Icon = "rbxassetid://104091122254403",
+	MaxStages = getMaxStages("Horned Melon")
+}
+t1.Cocoa = {
+	StageGrowTime = 52,
+	Price = 70000,
+	RollChance = 2E-06,
+	Cost = 10000000000,
+	Rarity = "Divine",
+	Icon = "rbxassetid://114725155379714",
+	MaxStages = getMaxStages("Cocoa")
+}
+t1.Kiwi = {
+	StageGrowTime = 54,
+	Price = 90000,
+	RollChance = 0,
+	Cost = 60000000000,
+	Rarity = "Exotic",
+	Icon = "rbxassetid://95748217573017",
+	MaxStages = getMaxStages("Kiwi")
+}
+t1.Moonflower = {
+	StageGrowTime = 58,
+	Price = 110000,
+	RollChance = 3.5E-06,
+	Cost = 70000000000,
+	Rarity = "Exotic",
+	Icon = "rbxassetid://135603884964310",
+	MaxStages = getMaxStages("Moonflower")
+}
+t1.Pepper = {
+	StageGrowTime = 60,
+	Price = 140000,
+	RollChance = 1.8E-06,
+	Cost = 900000000000,
+	Rarity = "Exotic",
+	Icon = "rbxassetid://92168908105918",
+	MaxStages = getMaxStages("Pepper")
+}
+t1["Void Fruit"] = {
+	StageGrowTime = 70,
+	Price = 180000,
+	RollChance = 9E-07,
+	Cost = 15000000000000,
+	Rarity = "Exotic",
+	Icon = "rbxassetid://107149331882043",
+	MaxStages = getMaxStages("Void Fruit")
+}
+t1.Dragonfruit = {
+	StageGrowTime = 55,
+	Price = 350000,
+	RollChance = 0,
+	Cost = 8000000000,
+	Rarity = "Exotic",
+	Icon = "rbxassetid://114062222095400",
+	MaxStages = getMaxStages("Dragonfruit")
+}
+t1.Passionfruit = {
+	StageGrowTime = 50,
+	Price = 120000,
+	RollChance = 2.5E-07,
+	Cost = 100000000000,
+	Rarity = "Exotic",
+	Icon = "rbxassetid://133826304800248",
+	MaxStages = getMaxStages("Passionfruit")
+}
+t1.Durian = {
+	StageGrowTime = 60,
+	Price = 380000,
+	RollChance = 5E-10,
+	Cost = 100000000000000,
+	Rarity = "Transcended",
+	Icon = "rbxassetid://136005865293840",
+	MaxStages = getMaxStages("Durian")
+}
+t1.Papaya = {
+	StageGrowTime = 57,
+	Price = 450000,
+	RollChance = 1E-10,
+	Cost = 150000000000000,
+	Rarity = "Transcended",
+	Icon = "rbxassetid://86766975895017",
+	MaxStages = getMaxStages("Papaya")
+}
+t1["Ghost Pepper"] = {
+	StageGrowTime = 55,
+	Price = 500000,
+	RollChance = 5E-12,
+	Cost = 275000000000000,
+	Rarity = "Transcended",
+	Icon = "rbxassetid://79932279226146",
+	MaxStages = getMaxStages("Ghost Pepper")
+}
+t1["Ember Fruit"] = {
+	StageGrowTime = 52,
+	Price = 525000,
+	RollChance = 5E-13,
+	Cost = 350000000000000,
+	Rarity = "Transcended",
+	Icon = "rbxassetid://122005165931520",
+	MaxStages = getMaxStages("Ember Fruit")
+}
+t1["Promise Lily"] = {
+	StageGrowTime = 7,
+	Price = 22,
+	RollChance = 0,
+	Cost = 2650,
+	Rarity = "Uncommon",
+	Icon = "rbxassetid://111529285577664",
+	MaxStages = getMaxStages("Promise Lily")
+}
+t1["Twinflame Tulip"] = {
+	StageGrowTime = 15,
+	Price = 450,
+	RollChance = 0,
+	Cost = 520000,
+	Rarity = "Epic",
+	Icon = "rbxassetid://83749180026101",
+	MaxStages = getMaxStages("Twinflame Tulip")
+}
+t1["Amulet Anemone"] = {
+	StageGrowTime = 26,
+	Price = 1800,
+	RollChance = 0,
+	Cost = 4500000,
+	Rarity = "Legendary",
+	Icon = "rbxassetid://99839924513909",
+	MaxStages = getMaxStages("Amulet Anemone")
+}
+t1["Duoheart Daisy"] = {
+	StageGrowTime = 47,
+	Price = 28000,
+	RollChance = 0,
+	Cost = 525000000,
+	Rarity = "Prismatic",
+	Icon = "rbxassetid://102842502980199",
+	MaxStages = getMaxStages("Duoheart Daisy")
+}
+t1["Heartvine Bloom"] = {
+	StageGrowTime = 60,
+	Price = 150000,
+	RollChance = 0,
+	Cost = 1000000000000,
+	Rarity = "Exotic",
+	Icon = "rbxassetid://113402139211947",
+	MaxStages = getMaxStages("Heartvine Bloom")
+}
+t1["Soulbound Orchid"] = {
+	StageGrowTime = 68,
+	Price = 500000,
+	RollChance = 0,
+	Cost = 275000000000000,
+	Rarity = "Transcended",
+	Icon = "rbxassetid://98392262882061",
+	MaxStages = getMaxStages("Soulbound Orchid")
+}
+t1.Honeysuckle = {
+	StageGrowTime = 14,
+	Price = 500,
+	RollChance = 0,
+	Cost = 615000,
+	Rarity = "Epic",
+	Icon = "rbxassetid://133873295727596",
+	MaxStages = getMaxStages("Honeysuckle")
+}
+t1["Martian Melon"] = {
+	StageGrowTime = 15,
+	Price = 620,
+	RollChance = 0,
+	Cost = 700000,
+	Rarity = "Epic",
+	Icon = "rbxassetid://106038784487820",
+	MaxStages = getMaxStages("Martian Melon")
+}
+t1["Truckers Delight"] = {
+	StageGrowTime = 62,
+	Price = 160000,
+	RollChance = 0,
+	Cost = 1100000000000,
+	Rarity = "Exotic",
+	Icon = "rbxassetid://104238821963098",
+	MaxStages = getMaxStages("Truckers Delight")
+}
+t1["Galaxy Hibiscus"] = {
+	StageGrowTime = 50,
+	Price = 32000,
+	RollChance = 0,
+	Cost = 1550000000,
+	Rarity = "Prismatic",
+	Icon = "rbxassetid://83921504164479",
+	MaxStages = getMaxStages("Galaxy Hibiscus")
+}
+t1["Queens Blossom"] = {
+	StageGrowTime = 70,
+	Price = 550000,
+	RollChance = 0,
+	Cost = 400000000000000,
+	Rarity = "Transcended",
+	Icon = "rbxassetid://139834284110532",
+	MaxStages = getMaxStages("Queens Blossom")
+}
+t1["Crimson Higanbana"] = {
+	StageGrowTime = 50,
+	Price = 225000,
+	RollChance = 0,
+	Cost = 35000000000000,
+	Rarity = "Exotic",
+	Icon = "rbxassetid://135675001780251",
+	MaxStages = getMaxStages("Crimson Higanbana")
+}
+t1["Striped Starfruit"] = {
+	StageGrowTime = 55,
+	Price = 130000,
+	RollChance = 0,
+	Cost = 120000000000,
+	Rarity = "Exotic",
+	Icon = "rbxassetid://131887947770411",
+	MaxStages = getMaxStages("Striped Starfruit")
+}
+t1["Rush Root"] = {
+	StageGrowTime = 50,
+	Price = 45000,
+	RollChance = 0,
+	Cost = 850000000,
+	Rarity = "Prismatic",
+	Icon = "rbxassetid://97782489653316",
+	MaxStages = getMaxStages("Rush Root")
+}
+t1["Compost Hydra"] = {
+	StageGrowTime = 52,
+	Price = 125000,
+	RollChance = 0,
+	Cost = 115000000000,
+	Rarity = "Divine",
+	Icon = "rbxassetid://124923772419773",
+	MaxStages = getMaxStages("Compost Hydra")
+}
+t1["Garden Devourer"] = {
+	StageGrowTime = 57,
+	Price = 350000,
+	RollChance = 0,
+	Cost = 80000000000000,
+	Rarity = "Transcended",
+	Icon = "rbxassetid://137021612636352",
+	MaxStages = getMaxStages("Garden Devourer")
+}
+t1["Garden Golem"] = {
+	StageGrowTime = 43,
+	Price = 565000,
+	RollChance = 0,
+	Cost = 425000000000000,
+	Rarity = "Transcended",
+	Icon = "rbxassetid://110990877607783",
+	MaxStages = getMaxStages("Garden Golem")
+}
+
+return t1
