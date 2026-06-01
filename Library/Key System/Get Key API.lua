5f61963187e3db51cad22bb19a0f71023b2a6055
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-06-01 04:36:20 +0400

    Update Get Key API.lua

--- Library/Key System/Get Key API.lua
+++ Library/Key System/Get Key API.lua
@@ -270,4 +270,21 @@ return {
       Icon = 137084367454843,
     },
   },
+
+	[10039338037] = { -- Build A Ring Farm
+    ["Linkvertise"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Linkvertise-jOqPFgJvjYNe",
+      Icon = 122637139040192,
+    },
+
+    ["LootLabs"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Lootlabs-cNYdzGiBsPbi",
+      Icon = 81598390322167,
+    },
+
+    ["Work.ink"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Workink-AaIaLIEtfopE",
+      Icon = 137084367454843,
+    },
+  },
 }
