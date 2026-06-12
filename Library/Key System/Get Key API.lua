d15afcb294467d61adc752eeb5ea726931805a3c
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-06-12 20:05:59 +0400

    Update Get Key API.lua

--- Library/Key System/Get Key API.lua
+++ Library/Key System/Get Key API.lua
@@ -304,4 +304,21 @@ return {
       Icon = 137084367454843,
     },
   },
+
+  [10200395747] = { -- Grow A Garden 2
+    ["Linkvertise"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Linkvertise-jOqPFgJvjYNe",
+      Icon = 122637139040192,
+    },
+
+    ["LootLabs"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Lootlabs-cNYdzGiBsPbi",
+      Icon = 81598390322167,
+    },
+
+    ["Work.ink"] = {
+      Url = "https://ads.luarmor.net/get_key?for=Speed_Hub_X_Workink-AaIaLIEtfopE",
+      Icon = 137084367454843,
+    },
+  },
 }
