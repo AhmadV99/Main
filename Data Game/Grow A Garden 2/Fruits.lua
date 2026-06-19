4e0513aa32a2aeea24349e968c1f8e871e596f44
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-06-19 15:55:52 +0400

    Create Fruits.lua

---
+++ Data Game/Grow A Garden 2/Fruits.lua
@@ -0,0 +1,363 @@
+return {
+  ["Grape"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Grape",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 2,
+      GrowRate = 0.0513,
+    },
+  },
+  ["Apple"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Apple",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.1167,
+    },
+  },
+  ["PartFruit"] = {
+    Extras = {
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 500,
+      GrowRate = 1,
+    },
+  },
+  ["Strawberry"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Apple",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1,
+      GrowRate = 0.3,
+    },
+  },
+  ["Blueberry"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Apple",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.15,
+      GrowRate = 0.2308,
+    },
+  },
+  ["Tomato"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Apple",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 0.9,
+      GrowRate = 0.2667,
+    },
+  },
+  ["Coconut"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Apple",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.0333,
+    },
+  },
+  ["Cactus"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Cactus",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.0119,
+    },
+  },
+  ["Mango"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Apple",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 3,
+      GrowRate = 0.031,
+    },
+  },
+  ["Cherry"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Cherry",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.0064,
+    },
+  },
+  ["Sunflower"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Sunflower",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 6,
+      GrowRate = 0.0061,
+    },
+  },
+  ["Acorn"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Acorn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.0058,
+    },
+  },
+  ["Beanstalk"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Beanstalk",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.06,
+    },
+  },
+  ["Poison Apple"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Fruit",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 2.25,
+      GrowRate = 0.0023,
+    },
+  },
+  ["Venus Fly Trap"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Venus Fly Trap",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 3,
+      GrowRate = 0.0013,
+    },
+  },
+  ["Thorn Rose"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Thorn Rose",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.5,
+    },
+  },
+  ["Pomegranate"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Pomegranate",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.0064,
+    },
+  },
+  ["Lotus"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Sunflower",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.007,
+    },
+  },
+  ["Pineapple"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Sunflower",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 5,
+      GrowRate = 0.01,
+    },
+  },
+  ["Poison Ivy"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Poison Ivy",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 2.1,
+      GrowRate = 0.0095,
+    },
+  },
+  ["Ghost Pepper"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Acorn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 7.5,
+      GrowRate = 0.0088,
+    },
+  },
+  ["Romanesco"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Acorn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.0119,
+    },
+  },
+  ["Baby Cactus"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Acorn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.4067,
+    },
+  },
+  ["Glow Mushroom"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Glow Mushroom",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 7,
+      GrowRate = 0.05,
+    },
+  },
+  ["Horned Melon"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Acorn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.125,
+      GrowRate = 0.0333,
+    },
+  },
+  ["Corn"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Corn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 3,
+      GrowRate = 0.0444,
+    },
+  },
+  ["Pinetree"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Apple",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.5,
+    },
+  },
+  ["Moon Bloom OLD"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Acorn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.03,
+    },
+  },
+  ["Banana"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Acorn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 1.5,
+      GrowRate = 0.0413,
+    },
+  },
+  ["Dragon Fruit"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Corn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 3,
+      GrowRate = 0.0061,
+    },
+  },
+  ["Dragon's Breath"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Acorn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 7.5,
+      GrowRate = 0.0078,
+    },
+  },
+  ["Moon Bloom"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Corn",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 9,
+      GrowRate = 0.0047,
+    },
+  },
+  ["Green Bean"] = {
+    Extras = {
+      Harvestable = true,
+      FruitType = "Beanstalk",
+    },
+    GrowData = {
+      GrowTickTime = NumberRange.new(4.800000190734863, 6),
+      BaseWeight = 0.5,
+      GrowRate = 0.06,
+    },
+  },
+}
